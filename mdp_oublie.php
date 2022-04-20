bc39f907755a8d4ef6ddec130ec3bec17f38df9e
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-04-20 16:56:50 +0200

    Update mdp_oublie.php

--- mdp_oublie.php
+++ mdp_oublie.php
@@ -1,52 +1,43 @@
 <?php include "template/header.php";?>
 
 <div class="row">
+	<div class="col-lg-2 col-md-1 col-sm-0"></div>
 
-			<div class="col-lg-2 col-md-1 col-sm-0"></div>
+	<div class="col-lg-8 col-md-10 col-sm-12 h-auto arrondie  ">
+		<div class="container py-2  h-auto  ">
+			<div class="row d-flex justify-content-center align-items-center h-100">
+				<div class="card bg-color text-white" style="border-radius: 1rem;">
+					<div class="card-body  text-center">
+						<div class="mb-md-5 mt-md-4 pb-5">
 
-			<div class="col-lg-8 col-md-10 col-sm-12 h-auto arrondie  ">
-					  <div class="container py-2  h-auto  ">
-					    <div class="row d-flex justify-content-center align-items-center h-100">
-					      
-					        <div class="card bg-color text-white" style="border-radius: 1rem;">
-					          <div class="card-body  text-center">
-					          	<div class="mb-md-5 mt-md-4 pb-5">
+							<h2 class="fw-bold mb-2 text-uppercase">Problèmes de connexion ?</h2>
+							<p class="text-white-50 mb-5"> Entrez votre adresse mail et nous vous enverrons un lien pour récupérer votre compte.</p>
 
-					              <h2 class="fw-bold mb-2 text-uppercase">Problèmes de connexion ?</h2>
-					              <p class="text-white-50 mb-5"> Entrez votre adresse mail et nous vous enverrons un lien pour récupérer votre compte.</p>
-
-					                <div class="row">
-
-						                <div class="col-lg-3 col-md-1 col-sm-0"></div>
-
-						                <div class="col-lg-6 col-md-12 col-sm-12">
-
-						                	<form method="POST" action="addUser.php">
-
-												<input type="email" class="form-control" name="email" placeholder="Votre email" required="required"><br>
-									  		</form>
-									 	</div>
-
-									 	<div class="col-lg-3 col-md-1 col-sm-0"></div>
-								  
-									</div>
-
-									<div class="row">
-
-										<div class="col-lg-4 col-md-1 col-sm-0"></div>
-										<div class="col-lg-4 col-md-12 col-sm-12">
-											<input type="submit" class="btn btn-outline-light btn-lg py-2 " value="Envoyer">
-										</div>
-										<div class="col-lg-4 col-md-1 col-sm-0"></div>
-
+							<div class="row">
+								<div class="col-lg-3 col-md-1 col-sm-0"></div>
+								<div class="col-lg-6 col-md-12 col-sm-12">
+									<form method="POST" action="addUser.php">
+										<input type="email" class="form-control" name="email" placeholder="Votre email" required="required"><br>
+									</form>
 								</div>
+								<div class="col-lg-3 col-md-1 col-sm-0"></div>
+							</div>
 
-					          </div>
-					        </div>
-					      </div>
-					    </div>
+							<div class="row">
+								<div class="col-lg-4 col-md-1 col-sm-0"></div>
+								<div class="col-lg-4 col-md-12 col-sm-12">
+									<input type="submit" class="btn btn-outline-light btn-lg py-2 " value="Envoyer">
+								</div>
+								<div class="col-lg-4 col-md-1 col-sm-0"></div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
-			<div class="col-lg-2 col-md-1 col-sm-0"></div>
+		</div>
+	</div>
+
+	<div class="col-lg-2 col-md-1 col-sm-0"></div>
 </div>
 
 			
