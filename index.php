3b0ccb9b50a2c7368585874ac71a2278831a6062
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-04-20 16:51:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,91 +1,74 @@
-<?php include "template/header.php";
-//QUentin ?>
+<?php include "template/header.php";?>
 
-		<!-- fin du header -->
-		<div class="row">
-
-			<div class="col-lg-2"></div>
-
-			<div class="col-lg-8 background-body py-5 h-middle ">
-				<div class="container py-5 ">
-
-
-		        <div class="row">
-		        	
-		        	<div class="col-lg-12 col-md-3 bg-color arrondie ">
-		                
-		                    <!-- <h1 class=" text-white text-center ">Tarte aux pommes</h1> -->
-		                    <div class="card-body">
-		                    	<!-- <img src="images/pomme.jpg" class="w-100 arrondie"> -->
-								<div class="d-flex justify-content-center">
-								  <div class="spinner-border" role="status">
-								    <span class="visually-hidden">Loading...</span>
-								  </div>
+<!-- fin du header -->
+	<div class="row">
+		<div class="col-lg-2"></div>
+		<div class="col-lg-8 background-body py-5 h-middle ">
+			<div class="container py-5 ">
+				<div class="row">
+					<div class="col-lg-12 col-md-3 bg-color arrondie ">
+						<!-- <h1 class=" text-white text-center ">Tarte aux pommes</h1> -->
+						<div class="card-body">
+							<!-- <img src="images/pomme.jpg" class="w-100 arrondie"> -->
+							<div class="d-flex justify-content-center">
+								<div class="spinner-border" role="status">
+									<span class="visually-hidden">Loading...</span>
 								</div>
-		                        <!-- <p class="lead mb-1 ">
-		            				<a href="#" class="btn btn-lg btn-secondary">En savoir plus</a>
-		        				</p> -->
-		                    </div>
-		            </div>
+							</div>
+							<!-- <p class="lead mb-1 ">
+								<a href="#" class="btn btn-lg btn-secondary">En savoir plus</a>
+							</p> -->
+						</div>
+					</div>
+				</div>
 
-
-		        </div>
-
-		        <div class="row my-5">
-		        	<div class="col-lg-4 col-md-3 bg-color arrondie px-5">
-
-		        		<!-- <h1 class=" text-white text-center ">Tarte aux pommes</h1> -->
-		                    <div class="card-body">
-		                    	<!-- <img src="images/pomme.jpg" class="w-100 arrondie"> -->
-								<div class="d-flex justify-content-center">
-								  <div class="spinner-border" role="status">
-								    <span class="visually-hidden">Loading...</span>
-								  </div>
+				<div class="row my-5">
+					<div class="col-lg-4 col-md-3 bg-color arrondie px-5">
+						<!-- <h1 class=" text-white text-center ">Tarte aux pommes</h1> -->
+						<div class="card-body">
+							<!-- <img src="images/pomme.jpg" class="w-100 arrondie"> -->
+							<div class="d-flex justify-content-center">
+									<div class="spinner-border" role="status">
+									<span class="visually-hidden">Loading...</span>
 								</div>
-		                        <!-- <p class="lead mb-1 ">
-		            				<a href="#" class="btn btn-lg btn-secondary">En savoir plus</a>
-		        				</p> -->
-		                    </div>
-		        	</div>
-		            <div class="col-lg-4 col-md-3 bg-color arrondie px-1">
-		                
-		                    <!-- <h1 class=" text-white text-center ">Tarte aux pommes</h1> -->
-		                    <div class="card-body">
-		                    	<!-- <img src="images/pomme.jpg" class="w-100 arrondie"> -->
-								<div class="d-flex justify-content-center">
-								  <div class="spinner-border" role="status">
-								    <span class="visually-hidden">Loading...</span>
-								  </div>
+							</div>
+							<!-- <p class="lead mb-1 ">
+								<a href="#" class="btn btn-lg btn-secondary">En savoir plus</a>
+							</p> -->
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-3 bg-color arrondie px-1">
+						<!-- <h1 class=" text-white text-center ">Tarte aux pommes</h1> -->
+						<div class="card-body">
+							<!-- <img src="images/pomme.jpg" class="w-100 arrondie"> -->
+							<div class="d-flex justify-content-center">
+								<div class="spinner-border" role="status">
+									<span class="visually-hidden">Loading...</span>
 								</div>
-		                        <!-- <p class="lead mb-1 ">
-		            				<a href="#" class="btn btn-lg btn-secondary">En savoir plus</a>
-		        				</p> -->
-		                    </div>
-		            </div>
-		            <div class="col-lg-4 col-md-3 bg-color arrondie px-1 ">
-		                
-		                    <!-- <h1 class=" text-white text-center ">Tarte aux pommes</h1> -->
-		                    <div class="card-body">
-		                    	<!-- <img src="images/pomme.jpg" class="w-100 arrondie"> -->
-								<div class="d-flex justify-content-center">
-								  <div class="spinner-border" role="status">
-								    <span class="visually-hidden">Loading...</span>
-								  </div>
+							</div>
+							<!-- <p class="lead mb-1 ">
+								<a href="#" class="btn btn-lg btn-secondary">En savoir plus</a>
+							</p> -->
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-3 bg-color arrondie px-1 ">
+						<!-- <h1 class=" text-white text-center ">Tarte aux pommes</h1> -->
+						<div class="card-body">
+							<!-- <img src="images/pomme.jpg" class="w-100 arrondie"> -->
+							<div class="d-flex justify-content-center">
+								<div class="spinner-border" role="status">
+									<span class="visually-hidden">Loading...</span>
 								</div>
-		                        <!-- <p class="lead mb-1 ">
-		            				<a href="#" class="btn btn-lg btn-secondary">En savoir plus</a>
-		        				</p> -->
-		                    </div>
-		            </div>
-		            
-
-
-		        </div>
-
-		        </div>
-		        </div>
-			<div class="col-lg-2"></div>
-
+							</div>
+							<!-- <p class="lead mb-1 ">
+								<a href="#" class="btn btn-lg btn-secondary">En savoir plus</a>
+							</p> -->
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
+		<div class="col-lg-2"></div>
+	</div>
 
 <?php include "template/footer.php";?>
