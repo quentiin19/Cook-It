8ef2cc93b6f4dc2de6d92d122533f5990045c0bc
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-05-01 15:34:06 +0200

    Update mdp_oublie.php

--- mdp_oublie.php
+++ mdp_oublie.php
@@ -1,45 +1,93 @@
+<?php 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+?>
 <?php include "template/header.php";?>
 
 <div class="row">
-	<div class="col-lg-2 col-md-1 col-sm-0"></div>
 
-	<div class="col-lg-8 col-md-10 col-sm-12 h-auto arrondie  ">
-		<div class="container py-2  h-auto  ">
-			<div class="row d-flex justify-content-center align-items-center h-100">
-				<div class="card bg-color text-white" style="border-radius: 1rem;">
-					<div class="card-body  text-center">
-						<div class="mb-md-5 mt-md-4 pb-5">
+			<div class="col-lg-2 col-md-1 col-sm-0"></div>
 
-							<h2 class="fw-bold mb-2 text-uppercase">Problèmes de connexion ?</h2>
-							<p class="text-white-50 mb-5"> Entrez votre adresse mail et nous vous enverrons un lien pour récupérer votre compte.</p>
+			<div class="col-lg-8 col-md-10 col-sm-12 h-auto arrondie  ">
+					  <div class="container py-2  h-auto  ">
+					    <div class="row d-flex justify-content-center align-items-center h-100">
+					      
+					        <div class="card bg-color text-white" style="border-radius: 1rem;">
+					          <div class="card-body  text-center">
+					          	<div class="mb-md-5 mt-md-4 pb-5">
 
-							<div class="row">
-								<div class="col-lg-3 col-md-1 col-sm-0"></div>
-								<div class="col-lg-6 col-md-12 col-sm-12">
-									<form method="POST" action="addUser.php">
-										<input type="email" class="form-control" name="email" placeholder="Votre email" required="required"><br>
-									</form>
+					              <h2 class="fw-bold mb-2 text-uppercase">Problèmes de connexion ?</h2>
+					              <p class="text-white-50 mb-5"> Entrez votre adresse mail et nous vous enverrons un lien pour récupérer votre compte.</p>
+
+					                <div class="row">
+
+						                <div class="col-lg-3 col-md-1 col-sm-0"></div>
+
+						                <div class="col-lg-6 col-md-12 col-sm-12">
+
+						                	<form method="POST" action="addUser.php">
+
+												<input type="email" class="form-control" name="email" placeholder="Votre email" required="required"><br>
+									  		</form>
+									 	</div>
+
+									 	<div class="col-lg-3 col-md-1 col-sm-0"></div>
+								  
+									</div>
+
+									<div class="row">
+
+										<div class="col-lg-4 col-md-1 col-sm-0"></div>
+										<div class="col-lg-4 col-md-12 col-sm-12">
+											<input type="submit" class="btn btn-outline-light btn-lg py-2 " value="Envoyer">
+										</div>
+										<div class="col-lg-4 col-md-1 col-sm-0"></div>
+
 								</div>
-								<div class="col-lg-3 col-md-1 col-sm-0"></div>
-							</div>
 
-							<div class="row">
-								<div class="col-lg-4 col-md-1 col-sm-0"></div>
-								<div class="col-lg-4 col-md-12 col-sm-12">
-									<input type="submit" class="btn btn-outline-light btn-lg py-2 " value="Envoyer">
-								</div>
-								<div class="col-lg-4 col-md-1 col-sm-0"></div>
-							</div>
-						</div>
-					</div>
-				</div>
+					          </div>
+					        </div>
+					      </div>
+					    </div>
 			</div>
-		</div>
-	</div>
-
-	<div class="col-lg-2 col-md-1 col-sm-0"></div>
+			<div class="col-lg-2 col-md-1 col-sm-0"></div>
 </div>
 
+
+<?php
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->Host = '51.255.172.36';               //Adresse IP ou DNS du serveur SMTP
+$mail->Port = 465;                          //Port TCP du serveur SMTP
+$mail->SMTPAuth = 1;                        //Utiliser l'identification
+$mail->CharSet = 'UTF-8';
+
+if($mail->SMTPAuth){
+   $mail->SMTPSecure = 'ssl';               //Protocole de sécurisation des échanges avec le SMTP
+   $mail->Username   =  'login@ovh.net';    //Adresse email à utiliser
+   $mail->Password   =  'password';         //Mot de passe de l'adresse email à utiliser
+}
+
+$mail->From       = trim($_POST["email_from"]);                //L'email à afficher pour l'envoi
+$mail->FromName   = trim($_POST["email_from_alias"]);          //L'alias de l'email de l'emetteur
+
+$mail->AddAddress(trim($_POST["email_to"]));
+
+$mail->Subject    =  $_POST["object"];                      //Le sujet du mail
+$mail->WordWrap   = 50; 			       //Nombre de caracteres pour le retour a la ligne automatique
+$mail->AltBody = $_POST["body"]; 	       //Texte brut
+$mail->IsHTML(false);                                  //Préciser qu'il faut utiliser le texte brut
+
+if (!$mail->send()) {
+      echo $mail->ErrorInfo;
+} else{
+      echo 'Message bien envoyé';
+}
+?>
 			
 
 
