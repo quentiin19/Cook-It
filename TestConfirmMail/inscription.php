08d79cc76a7cb638b71f1c50c536e059e89c1f7b
OussBth <103314967+OussBth@users.noreply.github.com>
2022-05-10 17:29:41 +0200

    Update inscription.php

--- TestConfirmMail/inscription.php
+++ TestConfirmMail/inscription.php
@@ -1,83 +1,60 @@
-<?php 
-require "PHPMailer/PHPMailerAutoload.php";
-session_start();
-//$bdd = new PDO('mysql:host=localhost;dbname=confirmation_email;','root','root');
 
-$email = "oussoumane.bathilyy@gmail.com";
+<?php
+require 'PHPMailer/PHPMailerAutoload.php';
 $cle = rand(1000000,9000000);
-        
-//  if(isset($_POST['valider'])){
 
-//      $insertUser = $bdd -> prepare('INSERT INTO users(email,cle,confirme) VALUES(?,?,?)');
-//      $insertUser->execute(array($email, $cle, 0));        
-//      $getUser = $bdd ->prepare('SELECT * FROM users WHERE email = ?');
-//      $getUser->execute(array($email));
-
-//  }  
-
-            
-            
 function smtpmailer($to, $from, $from_name, $subject, $body)
     {
         $mail = new PHPMailer();
-        
+        $mail->SMTPDebug = SMTP::DEBUG_CONNECTION;
         $mail->IsSMTP();
         $mail->SMTPAuth = true; 
- 
+        $mail->SMTPOptions = array(
+            'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+            )
+            );
         $mail->SMTPSecure = 'tls'; 
         $mail->Host = 'smtp.gmail.com';
         $mail->Port = 587;  
-        $mail->Username = 'projann2022@gmail.com';
+        $mail->Username = 'projann20222@gmail.com';
         $mail->Password = 'ProjAnn2022!.';   
    
    //   $path = 'reseller.pdf';
    //   $mail->AddAttachment($path);
    
         $mail->IsHTML(true);
-        $mail->From="projann2022@gmail.com";
+        $mail->From="projann20222@gmail.com";
         $mail->FromName=$from_name;
         $mail->Sender=$from;
         $mail->AddReplyTo($from, $from_name);
         $mail->Subject = $subject;
         $mail->Body = $body;
         $mail->AddAddress($to);
-        
         $mail->send();
         
-     if(!$mail->Send())
-    {
-        $error ="Essaye après chef ça bug";
-        return $error; 
-    }    
-    else 
-    {
-        $error = "Email envoyé";  
-        return $error;
-    }
-
+        if(!$mail->Send())
+        {
+            $error ="Please try Later, Error Occured while Processing...";
+            echo $error;
+            return $error; 
+        }
+        else 
+        {
+            $error = "Thanks You !! Your email is sent."; 
+            echo $error;
+            return $error;
+        }
     }
     
-    $to   = $email;
-    $from = 'projann2022@gmail.com';
+    $to   = 'oussoumane.bathilyy@gmail.com';
+    $from = 'projann20222@gmail.com';
     $name = 'Cook\'it';
     $subj = 'test Mail conf';
     $msg = 'http://localhost:3306/TestConfirmMail/verif.php?id='.$_SESSION['id'].'&cle='.$cle.'<h1>je suis ton père</h1>';
     
-    $error=smtpmailer($to,$from, $name ,$subj, $msg);
-
-
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Inscription</title>
-    <meta method="POST" action="">
-</head>
-<body>
-
-    <input type="submit" name="valider">
-
-</body>
+    smtpmailer($to,$from, $name ,$subj, $msg);
     
-</html>
+    ?>
