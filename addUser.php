bd410087a55a2b7d816c0300612cabd7e673aa49
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-05-22 18:16:30 +0200

    display error

--- addUser.php
+++ addUser.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require "functions.php";
 //require "./test/TestConfirmMail/inscription.php";
 
