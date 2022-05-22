113bfc1cf2e721caeb37e7f7a35c12b4873ad2c0
OussBth <103314967+OussBth@users.noreply.github.com>
2022-05-22 18:56:46 +0200

    Update addUser.php

--- addUser.php
+++ addUser.php
@@ -61,7 +61,7 @@ if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
 
 	//Vérification l'unicité de l'email
 	$pdo = connectDB();
-	$queryPrepared = $pdo->prepare("SELECT id from iw_user WHERE email=:email");
+	$queryPrepared = $pdo->prepare("SELECT ID from USER WHERE EMAIL=:email");
 
 	$queryPrepared->execute(["email"=>$email]);
 	
@@ -124,8 +124,8 @@ if(count($errors) == 0){
 	//$email = "y.skrzypczy@gmail.com";
 	//$firstname = "');DELETE FROM users;";
 
-	$queryPrepared = $pdo->prepare("INSERT INTO iw_user (email, firstname, lastname, pseudo, birthday, pwd) 
-		VALUES ( :email , :firstname, :lastname, :pseudo, :birthday, :pwd );");
+	$queryPrepared = $pdo->prepare("INSERT INTO USER (EMAIL, FIRSTNAME, LASTNAME, PSEUDO, HASHPWD) 
+		VALUES ( :email , :firstname, :lastname, :pseudo, :pwd );");
 
 
 	$pwd = password_hash($pwd, PASSWORD_DEFAULT);
@@ -135,7 +135,6 @@ if(count($errors) == 0){
 								"firstname"=>$firstname,
 								"lastname"=>$lastname,
 								"pseudo"=>$pseudo,
-								"birthday"=>$birthday,
 								"pwd"=>$pwd,
 	]);
 	
