44a3595a76c8cdd5dbf05a29abe111f936b9d4cb
OussBth <103314967+OussBth@users.noreply.github.com>
2022-05-22 17:41:35 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ function connectDB(){
 	//création d'une nouvelle connexion à notre bdd
 	try{
 		
-		$pdo = new PDO( "mariadb:host=localhost;dbname=ProjAnn;port=3306","admin" ,"QOY@BDD" );
+		$pdo = new PDO( "mariadb:host=51.255.172.36;dbname=ProjAnn;port=3306","admin" ,"QOY@BDD" );
 
     	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
