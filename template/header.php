614dc95965612470e17e823bf81c922d15c3f0b7
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-05-24 12:33:23 +0200

    script

--- template/header.php
+++ template/header.php
@@ -5,8 +5,7 @@
 	<!DOCTYPE html>
 	<html>
 	<head>
-		<script src="test/captcha-test/captcha2.js"></script>
-		<script src="test/captcha-test/tiles.js"></script>
+
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
