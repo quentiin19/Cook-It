0802359e5297c39d2d79f73b5e05daa5326f1241
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-05-26 17:19:04 +0200

    Update index.php

--- test/avatar-test/index.php
+++ test/avatar-test/index.php
@@ -5,10 +5,10 @@
     </head>
     <body>
         <div id="1">
-            <img src="img/image1.png" height="400px" width="400px"></img>
+            <img src="img/image1.png" height="300px" width="300px"></img>
         </div>
         <div id="2">
-            <img src="img/image2.png" height="400px" width="400px"></img>
+            <img src="img/image2.png" height="300px" width="300px"></img>
         </div>
         <button id="button">changer</button>
     </body>
