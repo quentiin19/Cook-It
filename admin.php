42dd063067d9da3722101f63297a549625f09b4b
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-05-26 15:33:25 +0200

    deluser

--- admin.php
+++ admin.php
@@ -66,27 +66,3 @@ include "template/footer.php";
 
 ?>
 
-<?php
-session_start();
-require "functions.php";
-
-//Vérification de l'utilisateur
-$id = $_GET["id"];
-if(!isConnected()){
-	die("Il faut se connecter !!!");
-}
-
-
-//Suppression du user en bdd
-$pdo = connectDB();
-$queryPrepared = $pdo->prepare("DELETE FROM USER WHERE id=:id");
-$queryPrepared->execute(["id"=>$id]);
-
-//Si c'est le user actuellement connecté je le deco
-if ($_SESSION['id'] == $id){
-	header("Location: logout.php");
-}
-
-
-//redirection sur la home
-header("Location: index.php");
