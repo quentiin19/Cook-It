211562287af78c73db3bbf53ef03047a481edc30
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-05-26 11:52:44 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -1,53 +1,55 @@
 <?php include "template/header.php";?>
 
-<?php 
+<div class="container">
+	<h1>Salut !</h1>
+
+	<?php 
 	if (isConnected()) {
 		$pdo = connectDB();
 
-		$queryPrepared = $pdo->prepare("SELECT * FROM iw_user");
+		$queryPrepared = $pdo->prepare("SELECT * FROM USER");
 		$queryPrepared->execute();
 		$results = $queryPrepared->fetchAll();
-?>
+		?>
 
-<div class="row">
 
-			<div class="col-lg-2 col-md-1 col-sm-0"></div>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>Id</th>
+					<th>Nom</th>
+					<th>Prénom</th>
+					<th>Pseudo</th>
+					<th>Email</th>
+				</tr>
+			</thead>
+			<tbody>
+				
+				<?php
+				foreach ($results as $user) {
+					echo '<tr>
+							<td>'.$user["id"].'</td>
+							<td>'.$user["lastname"].'</td>
+							<td>'.$user["firstname"].'</td>
+							<td>'.$user["pseudo"].'</td>
+							<td>'.$user["email"].'</td>
+							<td>
+								<div class="btn-group">
+									<a href="delUser.php?id='.$user["id"].'" class="btn btn-danger">Supprimer</a>
+									<a href="#" class="btn btn-warning" >Modifier</a>
+								</div>
+							</td>
+						</tr>';
+				}
+				?>
 
-			<div class="col-lg-8 col-md-10 col-sm-12 h-auto arrondie  ">
-					  <div class="container py-2  h-auto  ">
-					    <div class="row d-flex justify-content-center align-items-center h-100">
-					      
-					        <div class="card bg-color text-white" style="border-radius: 1rem;">
-					          <div class="card-body  text-center">
+									
 
-					            <div class="mb-md-5 mt-md-4 pb-5">
-					            	<div class="row">
-					            		<table class="table table-hover my-5 p-4 table-dark">
-								            <thead>
-								                <th scope="col">ID</th>
-								                <th scope="col">EMAIL</th>
-								                <th scope="col">USERNAME</th>
-								            </thead>
-								            <tbody>
-								                <?php
-								                for ($i = 0; $i < count($result); $i++) {
-								                    $user = $result[$i];
-								                    echo '<tr><th scope="row">' . $user["id"] . '</th>';
-								                    echo '<td>' . $user["email"] . '</td>';
-								                    echo '<td>' . $user["username"] . '</td>';
-								                }
-								                ?>
-								            </tbody>
-								        </table>
-					            	</div>
+			</tbody>
+		</table>
 
-					            </div>
-					           </div>
-					        </div>
-					    </div>
-					  </div>
-			</div>
-			<div class="col-lg-2 col-md-1 col-sm-0"></div>
-	</div>
+		<?php
+	}
+	?>
 
-<?php include "template/footer.php";?>
+</div>
