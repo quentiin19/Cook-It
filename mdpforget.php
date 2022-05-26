ad9666b0db4037d9f4562abc62a2e766a0420795
OussBth <103314967+OussBth@users.noreply.github.com>
2022-05-26 17:08:09 +0200

    Update mdpforget.php

--- mdpforget.php
+++ mdpforget.php
@@ -8,17 +8,20 @@
     <input type="email" class="form-control" name="password" placeholder="Votre Mot de passe" required="required"><br>
     <input type="email" class="form-control" name="passwordconfirm" placeholder="Confirmez votre Mot de passe" required="required"><br>
     
+    <div class="row">
+
+        <div class="col-lg-4 col-md-1 col-sm-0"></div>
+        <div class="col-lg-4 col-md-12 col-sm-12">
+            <input type="submit" class="btn btn-outline-light btn-lg py-2 " value="Envoyer">
+    </div>
+    
+    <div class="col-lg-4 col-md-1 col-sm-0"></div>
+
+</div>
+    
   </form>
 </div>
-<div class="row">
 
-<div class="col-lg-4 col-md-1 col-sm-0"></div>
-<div class="col-lg-4 col-md-12 col-sm-12">
-    <input type="submit" class="btn btn-outline-light btn-lg py-2 " value="Envoyer">
-</div>
-<div class="col-lg-4 col-md-1 col-sm-0"></div>
-
-</div>
     
 <?php
 
@@ -39,7 +42,7 @@
     $pwd = password_hash($pwd, PASSWORD_DEFAULT);
     
     if($_GET['id']==$_SESSION['id']){
-        if($_POST['cle']== $_SESSION['cle']){
+        if($_GET['cle']== $_SESSION['cle']){
             $queryPrepared = $pdo->prepare("UPDATE USER set HASHPWD =:pwd WHERE MAIL=:email AND ID =:id; ");
             $queryPrepared->execute(["pwd"=>$pwd,"email"=>$_SESSION['email'], "id"=>$_SESSION['id']]);
 
