99ee69b395903e19fc2a3a0904eada57ecc3e4ff
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-05-26 15:42:06 +0200

    Update header.php

--- template/header.php
+++ template/header.php
@@ -59,7 +59,7 @@
 				echo'<div class="col-lg-3 col-md-2 position-absolute  end-0 ">
 						<div class="text-align-center text-right>
 							<a href="./login.php" class="text-white">Se Connecter</a>
-							<p> / </p>
+							
 							<a href="./SignUp.php" class="text-white">S\'inscrire</a>	
 						</div> 
 				</div>';
