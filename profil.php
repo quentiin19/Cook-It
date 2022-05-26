d9bec685aa480f9e93a09a3bd0fd0607ed0d56f5
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-05-26 16:40:19 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -35,11 +35,11 @@ if (isConnected()) {
 									</div>
 								</div>
 								<div class="row">
-									<div class="col-lg-3 col-md-0 col-sm-0"></div>
-									<div class="col-lg-6 col-md-12 col-sm-12">
+									<div class="col-lg-2 col-md-0 col-sm-0"></div>
+									<div class="col-lg-8 col-md-12 col-sm-12">
 									<!-- Information du Compte -->
 										<form method="POST" action="">
-											<input type="text" class="form-control" name="firstname" placeholder="Votre prénom" value="<?=$results["FIRSTNAME"]?>"><br>
+											Votre prénom :<input type="text" class="form-control" name="firstname" placeholder="Votre prénom" value="<?=$results["FIRSTNAME"]?>"><br>
 											<input type="text" class="form-control" name="lastname" placeholder="Votre nom" value=" <?=$results["LASTNAME"]?>"><br>
 											<input type="text" class="form-control" name="pseudo" placeholder="Votre pseudo"  required="required" value=" <?=$results["PSEUDO"]?>"><br>
 					    			
@@ -50,7 +50,7 @@ if (isConnected()) {
 										<input type="text" class="form-control" name="firstname"  value="<?=$results["FIRSTNAME"]?>"><br>
 										</div>
 									</div>
-									<div class="col-lg-3 col-md-0 col-sm-0"></div>
+									<div class="col-lg-2 col-md-0 col-sm-0"></div>
 									
 									<?php 
 										
