f96002f9f074dcb86f6d7f783538fba7f324bd8c
OussBth <103314967+OussBth@users.noreply.github.com>
2022-05-26 13:50:07 +0200

    Update verif.php

--- test/TestConfirmMail/verif.php
+++ test/TestConfirmMail/verif.php
@@ -7,7 +7,7 @@ if($_GET['id']==$_SESSION['id']){
     if($_GET['cle']== $_SESSION['cle']){
         $queryPrepared = $pdo->prepare("UPDATE USER set role = 1 WHERE ID=:id ; ");
         $queryPrepared->execute(["id"=>$_SESSION['id']]);
-        header("Location : login.php");
+        // header("Location : login.php");
     }
     else
         echo "votre lien n'est plus valide";
