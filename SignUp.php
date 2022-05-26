db02c7e6f3880a0e755fe88b328d564dc9adb9b6
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-05-26 10:52:12 +0200

    Update SignUp.php

--- SignUp.php
+++ SignUp.php
@@ -46,11 +46,13 @@
 					</div>
 					<!-- Valider l'inscription -->
 					<div class="row">
-						<div class="col-lg-12 col-md-12 col-sm-12">
+						<div class="col-lg-5 col-md-5 col-sm-5"></div>
+						<div class="col-lg-2 col-md-2 col-sm-2">
 							<div class="text-center">
 	  						<input id="inscription" type="submit" class=" mt-5 btn btn-light btn-lg py-2 " value="S'inscrire">
+							</div>
 						</div>
-						</div>
+						<div class="col-lg-5 col-md-5 col-sm-5"></div>
 					</div>	
 					<div class="row">
 						<div class="col-lg-12 col-md-12 col-sm-12">
