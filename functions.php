2c9a7240b4b253723a140fb1dcd2db6136f77ebe
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-05-28 23:15:57 +0200

    update

--- functions.php
+++ functions.php
@@ -74,6 +74,13 @@ function isAdmin() {
 	
 }
 
+function updateLogs($id){
+	$pdo = connectDB();
+	$queryPrepared = $pdo->prepare("INSERT INTO LOGS (ID, DATE_LOGIN) VALUES (:id , GETDATE());");
+	$queryPrepared = $pdo->prepare("INSERT INTO LOGS (ID, DATE_LOGIN) VALUES (:id , CURRENT_TIMESTAMP);");
+	$queryPrepared->execute(["id"=>$id]);
+}
+
 
 ?>
 
