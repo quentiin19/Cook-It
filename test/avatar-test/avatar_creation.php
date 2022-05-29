f935f620ea38d5dffc658c47030820171430c3ef
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-05-29 21:24:42 +0200

    Update avatar_creation.php

--- test/avatar-test/avatar_creation.php
+++ test/avatar-test/avatar_creation.php
@@ -2,13 +2,13 @@
 
 
 
-$skin = imagecreatefrompng("img/skin".$_GET['skin'].".png");
+$avatar = imagecreatefrompng("img/skin".$_GET['skin'].".png");
 $eye = imagecreatefrompng("img/eye".$_GET['eye'].".png");
 $mouth = imagecreatefrompng("img/mouth".$_GET['mouth'].".png");
 
 
 
-$avatar = imagecopy($skin, $eye, 0, 0, 0, 0, 300, 300);
-$avatar = imagecopy($avatar, $mouth, 0, 0, 0, 0, 300, 300);
+imagecopy($avatar, $eye, 0, 0, 0, 0, 300, 300);
+imagecopy($avatar, $mouth, 0, 0, 0, 0, 300, 300);
 
 imagepng($avatar, "img/avatar.png");
