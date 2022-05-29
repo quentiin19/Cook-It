9ac599b7a0ba7b544cafe1c201e91034fa344599
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-05-29 16:43:59 +0200

    Update AddRecette.php

--- AddRecette.php
+++ AddRecette.php
@@ -27,6 +27,12 @@
 											</div>
 										</div>
 										<div class="row">
+											<div class="col-lg-12 col-md-12 col-sm-12">
+												<div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-offset="0" class="scrollspy-example" tabindex="0">
+												<p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
+												</div>
+											</div>
+										<div class="row">
 											<div class="col-lg-6 col-md-6">
 													<button class="btn btn-outline-light btn-lg px-2 " type="submit">Envoyer</button>
 											</div>
@@ -44,46 +50,7 @@
 </div>
 <?php include "template/footer.php";?>
 
-<!-- Upload d'image -->
 
-<?php
-	if(!empty($_FILES)){
-		$file_name = $_FILES['fichier']['name'];
-		$extension = strrchr($file_name, ".");
-
-		$file_path = $_FILES['fichier']['tmp_name'];
-		$destination = 'C:\MAMP\htdocs\Projet\fichier/'.$file_name;
-		$logo = imagecreatefrompng('images\bmw.png');
-		
-		
-		
-
-		$extension_authorised = array('.png', '.PNG', '.jpg', '.jpeg', '.JPG', '.JPEG');
-
-		
-		if(in_array($extension, $extension_authorised)){
-			//récupération des dimensions de l'image
-			$temp = getimagesize($file_name);
-
-			//création d'une canvas de mêmes dimensions que l'image
-			imagecreate($temp[0], $temp[1]);
-
-
-			imagecopy($img, $file_path, 0, 0, 0, 0, $temp[0], $temp[1]);
-			imagecopy($img, $logo, 20, 20, 0, 0, 250, 250);
-
-			if(move_uploaded_file($file_path, $destination)){
-				echo "Envoyé !";
-			}
-
-		}else{
-			echo "Veuillez rentrer choisir une image au format PNG, JPG ou JPEG";
-		}
-	}
-
-
-
-?>
 
 <?php
 if(
