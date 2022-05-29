11fa67ba79f5b00b058b4be2f086c8612319cf22
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-05-29 16:45:17 +0200

    Update AddRecette.php

--- AddRecette.php
+++ AddRecette.php
@@ -29,7 +29,16 @@
 										<div class="row">
 											<div class="col-lg-12 col-md-12 col-sm-12">
 												<div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-offset="0" class="scrollspy-example" tabindex="0">
-												<p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
+													<h4 id="scrollspyHeading1">First heading</h4>
+													<p>...</p>
+													<h4 id="scrollspyHeading2">Second heading</h4>
+													<p>...</p>
+													<h4 id="scrollspyHeading3">Third heading</h4>
+													<p>...</p>
+													<h4 id="scrollspyHeading4">Fourth heading</h4>
+													<p>...</p>
+													<h4 id="scrollspyHeading5">Fifth heading</h4>
+													<p>...</p>
 												</div>
 											</div>
 										<div class="row">
