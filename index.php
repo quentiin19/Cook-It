d1532101f0fd561f302f0cf7a7cc804aae431ded
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-05-30 11:49:46 +0200

    images

--- index.php
+++ index.php
@@ -11,13 +11,13 @@
 		  </div>
 		  <div class="carousel-inner">
 		    <div class="carousel-item active">
-		      <img src="./ressources/images/utilitaires/font.png" class="d-block w-100 "   alt="...">
+		      <img src="../ressources/images/utilitaires/font.png" class="d-block w-100 "   alt="...">
 		    </div>
 		    <div class="carousel-item">
-		      <img src="./ressources/images/utilitaires/font.png" class="d-block w-100" alt="...">
+		      <img src="../ressources/images/utilitaires/font.png" class="d-block w-100" alt="...">
 		    </div>
 		    <div class="carousel-item">
-		      <img src="./ressources/images/utilitaires/font.png" class="d-block w-100" alt="...">
+		      <img src="../ressources/images/utilitaires/font.png" class="d-block w-100" alt="...">
 		    </div>
 		  </div>
 		  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
