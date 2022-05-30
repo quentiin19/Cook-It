6b7041a08cd592a3be6d4fcc16f33840276ba6fa
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-05-30 15:47:35 +0200

    Update menu.php

--- test/header/ressources/menu.php
+++ test/header/ressources/menu.php
@@ -1,3 +1,37 @@
 <div id="burger-menu" hidden>
     Test
+    <div class="container-fluid ">
+        <nav class="navbar navbar-expand-md  ">
+            <div class="container-fluid">
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle text-white" data-bs-toggle="dropdown">Recettes</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#" class="dropdown-item">Viandes</a></li>
+                            <li><a href="#" class="dropdown-item">Légumes</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle text-white" data-bs-toggle="dropdown">Vos Abonnements</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#" class="dropdown-item">Test</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle text-white" data-bs-toggle="dropdown">Vos Favoris</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#" class="dropdown-item">Viandes</a></li>
+                            <li><a href="#" class="dropdown-item">Légumes</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item ">
+                        <a href="AddRecette.php" class="nav-link  text-white" >Vos Recettes</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
 </div>
