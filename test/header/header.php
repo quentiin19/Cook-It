ce11a4fc0a1e18cf92237120c247b6c4faff1bc1
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-05-31 15:43:59 +0200

    Update header.php

--- test/header/header.php
+++ test/header/header.php
@@ -29,11 +29,11 @@
                     <button id="burger-menu-button">Photo de profil</button>
 				</div>
 				
-				<div class="col-lg-8 col-sm-0 text-center align-self-center">
+				<div class="col-lg-8 col-sm-1 text-center align-self-center">
 					<h1>Cook'It</h1>
 				</div>
 
-				<div class="col-lg-2 col-sm-6">
+				<div class="col-lg-2 col-sm-5">
 					<a href="index.php"><button type="button" class="btn text-white btn-lg"><img src="../ressources/images/Utilitaires/logo.png" height ="80vh" width="100vw" />Cook'IT</button></a>
 				</div>
 
