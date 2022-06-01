e80ba469214f726f11c0ade63c72ca0c811bd6f3
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-01 19:38:19 +0200

    ajax

---
+++ test/ajax/api.php
@@ -0,0 +1,30 @@
+<?php
+
+require '../../functions.php';
+
+class API{
+    function ReturnRecipe($key_words){
+        //connexion à la base de données
+        $pdo = connectDB();
+
+        $array_key_words = explode('-', $key_words);
+
+        $queryPrepared = $pdo->prepare("SELECT * FROM RECIPES WHERE TITLE LIKE '%Boeuf%';");
+        $queryPrepared->execute();
+        $results = $queryPrepared->fetchAll();
+
+        
+
+
+
+        return json_encode($results);
+
+    }
+}
+
+$API = new API;
+header('Content-Type: application/json');
+echo $API->ReturnRecipe($_GET['keywords']);
+
+
+?>
