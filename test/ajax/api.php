311bfcd0d6da666d73c3aba72807e3cca946eaff
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-02 21:53:42 +0200

    Update api.php

--- test/ajax/api.php
+++ test/ajax/api.php
@@ -7,18 +7,39 @@ class API{
         //connexion à la base de données
         $pdo = connectDB();
 
+        //séparation de tous les mots dans un tableau
         $array_key_words = explode('-', $key_words);
-        $tarte = "tarte";
 
-        $queryPrepared = $pdo->prepare("SELECT * FROM RECIPES WHERE TITLE LIKE %:word%;");
-        $queryPrepared->execute(["word"=>$tarte]);
+        //pour chaque mots, on recherche dans la BDD si une recette correspond
+        foreach ($array_key_words as $index=>$word) {
+            $queryPrepared = $pdo->prepare("SELECT * FROM RECIPES WHERE TITLE LIKE :word;");
+            $queryPrepared->execute(["word"=>"%".$word."%"]);
+            $results[$index] = $queryPrepared->fetchAll();
+        }
+
+        for ($i = 0; $i < count($array_key_words); $i++) { 
+            
+        }
+
+        //clean de la recherche (retirer les recettes qui sont en plusieurs itérations)
+
+
+
+
+
+
+
+        /*
+        morceau de code pour récupérer les recettes en fonciton d'un mot
+        $queryPrepared = $pdo->prepare("SELECT * FROM RECIPES WHERE TITLE LIKE :word;");
+        $queryPrepared->execute(["word"=>"%".$tarte."%"]);
         $results = $queryPrepared->fetchAll();
-
+        */
         
 
 
-        print_r($results);
-        //return json_encode($results);
+        //print_r($results);
+        return json_encode($results);
 
     }
 }
