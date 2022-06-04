64b85d06e3eb9e042c456eb4a651e7b4f154c9f3
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-04 17:47:27 +0200

    e

--- test/avatar-test/avatar_creation.php
+++ test/avatar-test/avatar_creation.php
@@ -1,17 +1,32 @@
 <?php
 
+include "functions.php";
 
-
+//création des images dans la page php
 $avatar = imagecreatefrompng("img/skin".$_GET['skin'].".png");
 $eye = imagecreatefrompng("img/eye".$_GET['eye'].".png");
 $mouth = imagecreatefrompng("img/mouth".$_GET['mouth'].".png");
 
 
-
+//recopie des images sur l'image de fond ($avatar)
 imagecopy($avatar, $eye, 0, 0, 0, 0, 300, 300);
 imagecopy($avatar, $mouth, 0, 0, 0, 0, 300, 300);
 
-imagepng($avatar, "avatars/avatar.png");
+//création du fichier image qui portera comme nom l'id du user
+imagepng($avatar, "avatars/".$_SESSION['id'].".png");
+
+//libération de la mémoire
 imagedestroy($avatar);
 imagedestroy($eye);
 imagedestroy($mouth);
+
+//changement de base de données du chemin de l'avatar
+$path = "/var/www/html/ProjAnn/ressources/avatars/".$_SESSION['id'].".png";
+
+$pdo = connectDB();
+$queryPrepared = $pdo->prepare("INSERT INTO USER (PATH_AVATAR) VALUES (:path);");
+$queryPrepared->execute(["path"=>$path]);
+
+
+header("Location: index.php");
+
