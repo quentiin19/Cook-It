b62564a4497d467a4295d007ced7ef4e0d65c34a
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-05 12:31:53 +0200

    Update header.php

--- test/header/header.php
+++ test/header/header.php
@@ -25,13 +25,13 @@
 		<!-- Section menu haut -->
 
 		<div class=" bg-color  p-2 row align-self-center" >
-			<div class="col-lg-2 col-md-3 col-sm-6 align-self-center end-0 ">
+			<div class="col-lg-5 col-md-3 col-sm-6 align-self-center end-0 ">
 				<img id="burger-menu-button" src="../../ressources/images/avatars/<?=$_SESSION['id']?>.png" height="100vh" width="100vw">
 			</div>
-			<div class="col-lg-8 col-md-6 col-sm-0 text-center align-self-center">
+			<div class="col-lg-2 col-md-6 col-sm-0 text-center align-self-center">
 				<h1>Cook'It</h1>
 			</div>
-			<div class="col-lg-2 col-md-3 col-sm-5">
+			<div class="col-lg-5 col-md-3 col-sm-5">
 				<a href="index.php"><button type="button" class="btn text-white btn-lg"><img src="../ressources/images/Utilitaires/logo.png" height ="80vh" width="100vw" />Cook'IT</button></a>
 			</div>
 			
