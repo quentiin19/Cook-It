e19bf31cb9bac9509ac120bf28d90044d4505e6a
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-05 12:20:02 +0200

    Update menu.php

--- test/header/ressources/menu.php
+++ test/header/ressources/menu.php
@@ -1,11 +1,7 @@
 <div id="burger-menu" hidden>
     <div class="burger-section"><a href="">mes abonnements</a></div>
-    <br>
     <div class="burger-section"><a href="">mes recettes</a></div>
-    <br>
     <div class="burger-section"><a href="">modifier mon profil</a></div>
-    <br>
     <div class="burger-section"><a href="">modifier mon avatar</a></div>
-    <br>
     <div class="burger-section"><a href="">me déconnecter</a></div>
 </div>
