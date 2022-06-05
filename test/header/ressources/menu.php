9e5a432e6f729e5338c24d24b923e8243a2df27e
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-05 12:20:46 +0200

    d

--- test/header/ressources/menu.php
+++ test/header/ressources/menu.php
@@ -2,6 +2,7 @@
     <div class="burger-section"><a href="">mes abonnements</a></div>
     <div class="burger-section"><a href="">mes recettes</a></div>
     <div class="burger-section"><a href="">modifier mon profil</a></div>
+    
     <div class="burger-section"><a href="">modifier mon avatar</a></div>
     <div class="burger-section"><a href="">me déconnecter</a></div>
 </div>
