60d51f23c2956e1a04e17549f211bce6755a23b4
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-06 18:17:43 +0200

    Update test.php

--- test/test.php
+++ test/test.php
@@ -1,16 +1,5 @@
 <?php 
 
 
-require "../functions.php";
-
-
-$pdo = connectDB();
-
-$queryPrepared = $pdo->prepare("SELECT * FROM INGREDIENTS;");
-$queryPrepared->execute();
-$results = $queryPrepared->fetchAll();
-
-print "<pre>";
-print_r($results);
-print "</pre>";
+phpinfo();
 ?>
