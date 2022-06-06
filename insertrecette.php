c48adefefce09d2d86dff5dfea5e58d8b1c312a3
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-06 18:02:22 +0200

    Update insertrecette.php

--- insertrecette.php
+++ insertrecette.php
@@ -58,7 +58,7 @@ for ($i = 1; $i<6; $i++){
 
 if (!empty($_POST['fichier'])){
     print "<pre>";
-    print_r($_POST['fichier']);
+    print_r($_FILES);
     print "</pre>";
     
     if(!empty($_FILES)){
