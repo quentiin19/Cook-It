c3ebc3094a8f888299901e25cb1612d8ae1c9ec8
OussBth <103314967+OussBth@users.noreply.github.com>
2022-06-06 10:59:15 +0200

    Update AddRecette.php

--- AddRecette.php
+++ AddRecette.php
@@ -63,18 +63,18 @@
 
 
 <?php
-if(
-	empty($_POST["recette"]) || 
-	empty($_POST["recette_description"]) ||
-	empty($_POST["fichier"])||
-	count($_POST)!=3
-){
+// if(
+// 	empty($_POST["recette"]) || 
+// 	empty($_POST["recette_description"]) ||
+// 	empty($_POST["fichier"])||
+// 	count($_POST)!=3
+// ){
 
-	die("remplissez les champs requis");
+// 	die("remplissez les champs requis");
 
-}else{
-	echo "TEST";
-}
+// }else{
+// 	echo "TEST";
+// }
 
 
 $recette = $_POST["recette"];
