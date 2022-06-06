86ccc231a8f60c37f3d2fd25784a26f0dbc0fd76
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-06 16:09:18 +0200

    Update recette.php

--- recette.php
+++ recette.php
@@ -1,46 +1,65 @@
-<?php include "template/header.php";?>
+<?php include "template/header.php";
+
+$pdo = connectDB();
+$queryPrepared = $pdo->prepare("SELECT * FROM RECIPES WHERE ID = :id;");
+$queryPrepared->execute(["id"=>$_GET['id']]);
+$recipe = $queryPrepared->fetch();
+
+
+$queryPrepared = $pdo->prepare("SELECT * FROM NEED WHERE ID_RECIPE = :id;");
+$queryPrepared->execute(["id"=>$_GET['id']]);
+$needs = $queryPrepared->fetch();
+
+print '<pre>';
+print_r($needs);
+print '</pre>';
+
+
+?>
 
 
 <div class="row">
 
-			<div class="col-lg-2 col-md-1 col-sm-0"></div>
-
-			<div class="col-lg-8 col-md-10 col-sm-12 h-auto arrondie  ">
-					  <div class="container py-2  h-auto  ">
-					    <div class="row d-flex justify-content-center align-items-center h-100">
-					      
-					        <div class="card bg-color text-white" style="border-radius: 1rem;">
-					          <div class="card-body  text-center">
-
-					            <div class="mb-md-5 mt-md-4 pb-5">
-									<h2 class="fw-bold mb-2 text-uppercase">Nom de la recette</h2>
-								
-									<div class="row">
-										<div class="col-lg-12 col-md-12 col-md-12">
-											<img src="">
-										</div>
-									</div>
-									<div class="row">
-										<div class="col-lg-8 col-md-8 col-sm-8">
-											<h4>Recette :</h4> <br>
-										</div>
-										<div class="col-lg-4 col-md-4 col-sm-4">
-											<h4>Ingredients :</h4> <br>
-											<table>
-												<tr>
-													<td>test</td>
-													<td>test</td>
-												</tr>
-											</table>
-										</div>
-									</div>	
-					            </div>
-					           </div>
-					        </div>
-					    </div>
-					  </div>
+	<div class="col-lg-2 col-md-1 col-sm-0"></div>
+	<div class="col-lg-8 col-md-10 col-sm-12 h-auto arrondie  ">
+		<div class="container py-2  h-auto  ">
+			<div class="row d-flex justify-content-center align-items-center h-100">
+				<div class="card bg-color text-white" style="border-radius: 1rem;">
+					<div class="card-body  text-center">
+						<div class="mb-md-5 mt-md-4 pb-5">
+							<h2 class="fw-bold mb-2 text-uppercase"><?= $recipe['TITLE']?></h2>
+							<div class="row">
+								<div class="col-lg-12 col-md-12 col-md-12">
+									<img src="">
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-lg-8 col-md-8 col-sm-8">
+									<h4>Recette :</h4><br>
+									<p><?= $recipe['DESCRIPTION']?></p>
+								</div>
+								<div class="col-lg-4 col-md-4 col-sm-4">
+									<h4>Ingredients :</h4> <br>
+									<table>
+									<?php
+										foreach ($ingredients as $key => $ingredient) {
+											
+											echo '	<tr>
+														<td>'.$ingredient[].'</td>
+														<td>test</td>
+													</tr>';
+										}
+										?>
+									</table>
+								</div>
+							</div>	
+						</div>
+					</div>
+				</div>
 			</div>
-			<div class="col-lg-2 col-md-1 col-sm-0"></div>
+		</div>
+	</div>
+	<div class="col-lg-2 col-md-1 col-sm-0"></div>
 </div>
 
 
