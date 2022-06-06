a9636b1c9e7356e60e779d1f8af46b6b308fa7c5
OussBth <103314967+OussBth@users.noreply.github.com>
2022-06-06 17:30:22 +0200

    Update insertrecette.php

--- insertrecette.php
+++ insertrecette.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require 'functions.php';
+require "functions.php";
 // echo "testeurer";
 // if(
 // 	empty($_POST["recette"]) || 
@@ -26,7 +26,7 @@ if (!empty($_POST['fichier'])){
         $file_path = $_FILES['fichier']['tmp_name'];
 
         //destination que l'on souhaite pour fichier
-        $destination = '/var/www/html/ProjAnn/test/upload-image/uploaded_images/';
+        $destination = '/var/www/html/ProjAnn/ressources/images/images-recettes/';
 
         //extention du fichier
         $extension = strrchr($file_name, ".");
@@ -39,12 +39,12 @@ if (!empty($_POST['fichier'])){
         //si le fichier est une image autorisé
         if(in_array($extension, $extension_authorised)){
             echo "test";
-        
+            // sert à bouger le fichier qui vient d'être upload dans la destination que l'on veut
             if(move_uploaded_file($_FILES['fichier']['tmp_name'], $destination.$file_name)){
                 echo "Envoyé !";
 
                 //création du filigranne
-                $logo = imagecreatefrompng('sources/logo.png');
+                $logo = imagecreatefrompng('ressource/images/Utilitaires/logo.png');
 
                 //création de l'image de base
                 $img = imagecreatefrompng($destination.$file_name);
@@ -57,7 +57,7 @@ if (!empty($_POST['fichier'])){
                 imagecopy($final_img, $logo, 0, 0, 0, 0, 50, 50);
 
                 //nom final du fichier (id de la recette et index de l'image) - A CHANGER
-                $final_file_name = "1_1.png";
+                $final_file_name = md5(sha1($_POST['recette'].$_POST['recette_description']).uniqid()."lavida").".png";
 
                 //suppression de l'ancien fichier
                 unlink($destination.$file_name);
@@ -91,15 +91,17 @@ $pdo = connectDB();
 $queryPrepared = $pdo->prepare("INSERT INTO RECIPES (        
 ID_CREATOR, 
 TITLE,      
-DESCRIPTION ) 
-VALUES (:idcreator, :title, :recettedesc);");
+DESCRIPTION,
+PICTURE_PATH) 
+VALUES (:idcreator, :title, :recettedesc, :path);");
 
 
 
 $queryPrepared->execute([
 						"idcreator"=>$_SESSION['id'],
 						"title"=>$recette,
-						"recettedesc"=>$recette_description
+						"recettedesc"=>$recette_description,
+						"path"=>$final_file_name
 ]);
 // header("Location:recette.php");
 
@@ -108,14 +110,10 @@ $queryPrepared = $pdo->prepare("SELECT ID FROM RECIPES WHERE ID_CREATOR=:id AND 
 $queryPrepared->execute(["id"=>$_SESSION['id'], "title"=>$recette]);
 $result = $queryPrepared->fetch();
 
-//insertion image dans la table recipes
-$queryPrepared = $pdo->prepare("UPDATE RECIPES set PICTURE_PATH = WHERE ID=:id AND TITLE=:recette");
-$queryPrepared->execute(["id"=>$result['ID'], "recette"=>$recette]);
-
-
 //insertion ingrédient dans la table NEED
 for ($i = 1; $i<6; $i++){
-    if(isset($_POST['checkbox'.$i])){
+    $index = 'checkbox'.$i;
+    if($_POST[$index]){
         $quantity = $_POST["quantity".$i];
         $queryPrepared = $pdo->prepare("INSERT INTO NEED VALUES (:quantity, :id_ingr, :id_recipe)");
         $queryPrepared->execute(["quantity"=>$quantity, "id_ingr"=>$i ,"id_recipe"=>$result['ID']]);
