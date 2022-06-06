e9afd0fb24326b9bd76cd1a4eeffaa07389fe6b7
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-06 12:50:43 +0200

    Update api.php

--- test/ajax/api.php
+++ test/ajax/api.php
@@ -76,14 +76,16 @@ class API{
                 }
             }
 
-            array_push($result, $recipes[$index_max][0]);
+            array_push($result, $recipes[$index_max]);
             unset($recipes[$index_max]);
             unset($pertinence[$index_max]);
 
         }
         
 
-        
+        print "<pre>";
+        print_r($result);
+        print "</pre>";
 
         
 
