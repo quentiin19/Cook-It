edadcddcb42e3abbd4777e710bc8f0a91dc87e03
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-06 13:19:30 +0200

    Update api.php

--- test/ajax/api.php
+++ test/ajax/api.php
@@ -60,16 +60,6 @@ class API{
                     }
                 }
             }
-            /*
-            for ($i = 0; $i < count($recipes); $i++) { 
-                if (isset($recipes[$i])) {
-                    if ($pertinence[$i] >= $max_pert) {
-                        $index_max = $i;
-                        $max_pert = $pertinence[$i];
-                        $max_reci = $recipes[$i];
-                    }
-                }
-            }*/
 
             array_push($result, $recipes[$index_max]);
             unset($recipes[$index_max]);
@@ -81,110 +71,7 @@ class API{
             }
 
         }
-        
-
-        
-
-        /*
-        print "<pre>";
-        print_r($queryResults);
-        print "</pre>";
-
-        $temp = $queryResults[0][0][0];
-        $queryResults[0][0][0] = $queryResults[0][1][0];
-        $queryResults[0][1][0] = $temp;
-
-
-
-
-        $results = array();
-        
-
-        //clean de la recherche (retirer les recettes qui sont en plusieurs itérations)
-        for ($i = 0; $i < count($queryResults); $i++) { 
-            for ($j = 0; $j < count($queryResults[$i]); $j++) { 
-
-                //Si l'id de la recette existe déjà dans le tableau de résultats
-                if ($results[$queryResults[$i][$j][0]] != NULL) {
-                    //on rajoute 1 à la pertinence de la recherche
-                    $results[$queryResults[$i][$j][0]][1] += 1;
-
-                //Sinon, on l'insert dans le tableau de résultats
-                }else {
-                    $temp1 = array($queryResults[$i][$j][0], 1);
-                    $temp2 = array($queryResults[$i][$j][0] => $temp1);
-
-                    array_merge($results, $temp2);
-                }
-            }
-        }
-
-        //fonction de swap
-        function swap($a, $b){
-            $temp = $a;
-            $a = $b;
-            $b = $temp;
-        }
-
-        //fonction pour remettre tous les index de manière claire
-        function index_clean($array){
-            $i = 0;
-            foreach ($array as $key => $value) {
-                $key = $i;
-                $i++;
-            }
-        }
-
-        //fonction pour savoir si le tableau $results est trié
-        function is_sort($array){
-            index_clean($array);
-
-            for ($i = 1; $i < count($array); $i++) { 
-                //si la deuxième recherche est plus pertinente que la première, le tableau n'est pas trié
-                if ($array[$i - 1][1] < $array[$i][1]) {
-                    return false;
-                }
-            }
-        }
-
-        //enfin, nous trions le table du plus pertinent au moin pertinent
-        
-        while (is_sort($results)) {
-            for ($i = 0; $i < count($results); $i++) { 
-                if ($array[$i - 1][1] < $array[$i][1]) {
-                    swap($array[$i - 1][1], $array[$i][1]);
-                }
-            }
-        }
-        */
-
-
-
-
-
-
-
-
-        /*
-        morceau de code pour récupérer les recettes en fonciton d'un mot
-        $queryPrepared = $pdo->prepare("SELECT * FROM RECIPES WHERE TITLE LIKE :word;");
-        $queryPrepared->execute(["word"=>"%".$tarte."%"]);
-        $results = $queryPrepared->fetchAll();
-        
-        print "<pre>";
-        print_r($queryResults);
-        print "</pre>";
-        
-        print "<pre>";
-        print_r($results);
-        print "</pre>";
-        */
-        
-
-        
         return json_encode($result);
-
-
     }
 }
 
