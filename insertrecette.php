fce7f503aca55ab6fb54685a42fffbe311b1e143
OussBth <103314967+OussBth@users.noreply.github.com>
2022-06-06 11:24:43 +0200

    insert

---
+++ insertrecette.php
@@ -0,0 +1,35 @@
+<?php
+// if(
+// 	empty($_POST["recette"]) || 
+// 	empty($_POST["recette_description"]) ||
+// 	empty($_POST["fichier"])||
+// 	count($_POST)!=3
+// ){
+
+// 	die("remplissez les champs requis");
+
+// }else{
+// 	echo "TEST";
+// }
+
+
+$recette = $_POST["recette"];
+$recette_description = $_POST["recette_description"];
+$fichier = $_POST["fichier"];
+
+$queryPrepared = $pdo->prepare("INSERT INTO RECIPES (        
+ID_CREATOR, 
+TITLE,      
+DESCRIPTION ) 
+VALUES (:idcreator, :title, :recettedesc);");
+
+
+
+$queryPrepared->execute([
+						"idcreator"=>$_SESSION['ID'],
+						"title"=>$recette,
+						"recettedesc"=>$recette_description
+]);
+
+
+?>
