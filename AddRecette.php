662f4824ab7cbae87699109c4ba08d95f02ed190
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-06-06 11:14:46 +0200

    Update AddRecette.php

--- AddRecette.php
+++ AddRecette.php
@@ -37,7 +37,7 @@
 											foreach ($results as $key => $ingredient) { ?>
 											<div class="row">
 											<div class="col-lg-2 col-md-2 col-sm-2"></div>
-											<div class="col-lg-8 col-md-8 col-sm-8 background-body arrondie">
+											<div class="col-lg-8 col-md-8 col-sm-8 background-body arrondie my-2">
 												<div class="row align-items-center">
 														<div class="col-lg-1 col-md-1 col-sm-1">
 															<input  type="checkbox" name="checkbox">
