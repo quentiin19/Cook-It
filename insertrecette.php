daaac08bc89721afb022092aad6b362983910da0
OussBth <103314967+OussBth@users.noreply.github.com>
2022-06-06 11:34:06 +0200

    Update insertrecette.php

--- insertrecette.php
+++ insertrecette.php
@@ -19,6 +19,7 @@ $recette = $_POST["recette"];
 $recette_description = $_POST["recette_description"];
 $fichier = $_POST["fichier"];
 
+$pdo = connectDB();
 $queryPrepared = $pdo->prepare("INSERT INTO RECIPES (        
 ID_CREATOR, 
 TITLE,      
