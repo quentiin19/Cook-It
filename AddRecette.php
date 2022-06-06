ac7088c58242792257e3333378096eae4beb81c0
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-06-06 14:49:43 +0200

    Update AddRecette.php

--- AddRecette.php
+++ AddRecette.php
@@ -52,10 +52,10 @@
 														<div class="col-lg-3 col-md-3 col-sm-3">
 															<p ><?= $ingredient['NAME']?></p>
 														</div>
-														<div class="col-lg-3 col-md-3 col-sm-3">
+														<div class="col-lg-3 col-md-2 col-sm-2">
 															<input type="text" name="quantity<?php echo $ingredient['ID']?>" placeholder="quantité">
 														</div>
-														<div class="col-lg-2 col-md-2 col-sm-2">
+														<div class="col-lg-2 col-md-3 col-sm-3">
 															<?= $ingredient['UNIT']?>
 														</div>		
 												</div>
