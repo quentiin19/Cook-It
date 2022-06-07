e1966c79a562a5cf115471f3b91f275c39908545
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-07 14:27:46 +0200

    Update AddRecette.php

--- AddRecette.php
+++ AddRecette.php
@@ -97,38 +97,6 @@ print "</pre>";
 if(!empty($_POST)){
 	//création du nom de l'image
 	$final_file_name = md5(sha1($_POST['recette'].$_POST['recette_description']).uniqid()."lavida").".png";
-	echo $final_file_name;
-
-	//insertion recette dans tables recipes
-	$recette = $_POST["recette"];
-	$recette_description = $_POST["recette_description"];
-
-	$pdo = connectDB();
-	$queryPrepared = $pdo->prepare("INSERT INTO RECIPES (ID_CREATOR, TITLE, DESCRIPTION, PICTURE_PATH) VALUES (:idcreator, :title, :recettedesc, :path);");
-
-
-
-	$queryPrepared->execute([
-							"idcreator"=>$_SESSION['id'],
-							"title"=>$recette,
-							"recettedesc"=>$recette_description,
-							"path"=>$final_file_name
-	]);
-
-
-	$queryPrepared = $pdo->prepare("SELECT ID FROM RECIPES WHERE ID_CREATOR=:id AND TITLE=:title");
-	$queryPrepared->execute(["id"=>$_SESSION['id'], "title"=>$recette]);
-	$result = $queryPrepared->fetch();
-
-	//insertion ingrédient dans la table NEED
-	for ($i = 1; $i<6; $i++){
-		if(isset($_POST['checkbox'.$i])){
-			$quantity = $_POST["quantity".$i];
-			$queryPrepared = $pdo->prepare("INSERT INTO NEED VALUES (:quantity, :id_ingr, :id_recipe)");
-			$queryPrepared->execute(["quantity"=>$quantity, "id_ingr"=>$i ,"id_recipe"=>$result['ID']]);
-		}
-	}
-
 		
 	if(!empty($_FILES)){
 		//enregistrement de l'image sur le serveur
@@ -147,11 +115,8 @@ if(!empty($_POST)){
 		//liste des extentions autorisées à être uploadées
 		$extension_authorised = array('.png', '.PNG', '.jpg', '.jpeg', '.JPG', '.JPEG');
 
-		echo "test";
-
 		//si le fichier est une image autorisé
 		if(in_array($extension, $extension_authorised)){
-			echo "test";
 			// sert à bouger le fichier qui vient d'être upload dans la destination que l'on veut
 			if(move_uploaded_file($_FILES['fichier']['tmp_name'], $destination.$file_name)){
 				echo "Envoyé !";
@@ -186,6 +151,36 @@ if(!empty($_POST)){
 				
 		}
 	
+	}
+
+	//insertion recette dans tables recipes
+	$recette = $_POST["recette"];
+	$recette_description = $_POST["recette_description"];
+
+	$pdo = connectDB();
+	$queryPrepared = $pdo->prepare("INSERT INTO RECIPES (ID_CREATOR, TITLE, DESCRIPTION, PICTURE_PATH) VALUES (:idcreator, :title, :recettedesc, :path);");
+
+
+
+	$queryPrepared->execute([
+							"idcreator"=>$_SESSION['id'],
+							"title"=>$recette,
+							"recettedesc"=>$recette_description,
+							"path"=>$destination.$final_file_name
+	]);
+
+
+	$queryPrepared = $pdo->prepare("SELECT ID FROM RECIPES WHERE ID_CREATOR=:id AND TITLE=:title");
+	$queryPrepared->execute(["id"=>$_SESSION['id'], "title"=>$recette]);
+	$result = $queryPrepared->fetch();
+
+	//insertion ingrédient dans la table NEED
+	for ($i = 1; $i<6; $i++){
+		if(isset($_POST['checkbox'.$i])){
+			$quantity = $_POST["quantity".$i];
+			$queryPrepared = $pdo->prepare("INSERT INTO NEED VALUES (:quantity, :id_ingr, :id_recipe)");
+			$queryPrepared->execute(["quantity"=>$quantity, "id_ingr"=>$i ,"id_recipe"=>$result['ID']]);
+		}
 	}
 
 
