c2915937dd8a64adf2996fc994094ae1bfa65148
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-07 20:23:16 +0200

    suppression d'un user

--- delUser.php
+++ delUser.php
@@ -2,24 +2,103 @@
 session_start();
 require "functions.php";
 
+//Ordre des BDD où il faut delete les données : 
+/*
+- MESSAGE -> Supprimer tous les messages qui implique le user
+- SUBSCRIPTION -> supprimer tous les abonnements du user
+- FRIDGE -> supprimer tous les ingrédients dans le frigo du user
+- RECIPES_SAVED -> supprimer toutes les recettes sauvegardées par le user
+- PICTURES -> supprimer toutes les images dans qui appartiennent à des recettes créé par le user
+- NEED -> supprimer tous les besions des recettes créé par le user
+- VOTES -> supprimer tous les votes du user
+- RECIPES -> supprimer toutes les recettes créé par le user
+- LOGS -> supprimer tous les logs du user
+- USER -> enfin supprimer le user dans la table USER
+
+
+*/
+
+
+
 //Vérification de l'utilisateur
- $id = $_GET["id"];
-if(!isConnected()){
-	die("Il faut se connecter !!!");
+if(isAdmin() || $_GET['id'] == isConnected()){
+	//connexion à la base de données
+	$pdo = connectDB();
+
+
+
+	//suppression des messages
+	$queryPrepared = $pdo->prepare("DELETE * FROM MESSAGE WHERE ID_SENDER=:id OR ID_RECEIVER=:id;");
+	$queryPrepared->execute(["id"=>$id]);
+
+
+
+	//suppression des abonnements
+	$queryPrepared = $pdo->prepare("DELETE * FROM SUBSCRIPTION WHERE ID_SUBSCRIPTION=:id OR ID_SUBSCRIBER=:id;");
+	$queryPrepared->execute(["id"=>$id]);
+
+
+
+	//suppression du frigo
+	$queryPrepared = $pdo->prepare("DELETE * FROM FRIDGE WHERE ID_USER=:id;");
+	$queryPrepared->execute(["id"=>$id]);
+
+
+
+	//suppression des recettes que le user à sauvegardé
+	$queryPrepared = $pdo->prepare("DELETE * FROM RECIPES_SAVED WHERE ID_USER=:id;");
+	$queryPrepared->execute(["id"=>$id]);
+
+	//suppression des recettes que le user à créé et que d'autres ont sauvegardé
+	$queryPrepared = $pdo->prepare("DELETE * FROM RECIPES_SAVED WHERE ID_RECIPE IN (SELECT * FROM RECIPES WHERE ID_CREATOR=:id);");
+	$queryPrepared->execute(["id"=>$id]);
+
+
+
+	//suppression des images dans les recettes que le user à créé
+	$queryPrepared = $pdo->prepare("DELETE * FROM PICTURE WHERE ID_RECIPE IN (SELECT * FROM RECIPES WHERE ID_CREATOR=:id);");
+	$queryPrepared->execute(["id"=>$id]);
+
+
+
+	//suppression des besoin des recettes créé par le user
+	$queryPrepared = $pdo->prepare("DELETE * FROM NEED WHERE ID_RECIPE IN (SELECT * FROM RECIPES WHERE ID_CREATOR=:id);");
+	$queryPrepared->execute(["id"=>$id]);
+
+
+
+	//suppression des votes du user
+	$queryPrepared = $pdo->prepare("DELETE * FROM VOTES WHERE ID_USER=:id;");
+	$queryPrepared->execute(["id"=>$id]);
+
+	//suppression des votes sur les recettes créé par le user
+	$queryPrepared = $pdo->prepare("DELETE * FROM VOTES WHERE ID_RECIPE IN (SELECT * FROM RECIPES WHERE ID_CREATOR=:id);");
+	$queryPrepared->execute(["id"=>$id]);
+
+
+
+	//suppression des recettes créées par le user
+	$queryPrepared = $pdo->prepare("DELETE * FROM RECIPES WHERE ID_CREATOR=:id;");
+	$queryPrepared->execute(["id"=>$id]);
+
+
+
+	//suppression des logs du user
+	$queryPrepared = $pdo->prepare("DELETE * FROM LOGS WHERE ID=:id;");
+	$queryPrepared->execute(["id"=>$id]);
+
+
+
+	//suppression du user lui-même
+	$queryPrepared = $pdo->prepare("DELETE * FROM USER WHERE ID=:id;");
+	$queryPrepared->execute(["id"=>$id]);
+
+
+
+	//redirection sur la home
+	header("Location: index.php");
+
+}else{
+	die("Vous ne pouvez pas supprimer ce compte.");
 }
 
-
-//Suppression du user en bdd
-$pdo = connectDB();
-$queryPrepared = $pdo->prepare("DELETE FROM USER WHERE id=:id");
-$queryPrepared->execute(["id"=>$id]);
-updateLogs($id, 'suppression');
-
-//Si c'est le user actuellement connecté je le deco
-if ($_SESSION['id'] == $id){
-	header("Location: logout.php");
-}
-
-
-//redirection sur la home
-header("Location: index.php");
--- deleteAll.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-require "functions.php";
-
-
-//Suppression des recettes du user en bdd
-$pdo = connectDB();
-$queryPrepared = $pdo->prepare("DELETE FROM USER WHERE id=:id");
-$queryPrepared->execute(["id"=>$id]);
