306220b1c4d35c6217282f812b9ee75e90f6fb3c
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-07 15:11:38 +0200

    Create affichagerecettes.php

---
+++ affichagerecettes.php
@@ -0,0 +1,19 @@
+<?php require 'template/header.php';?>
+
+
+<div class="row">
+    <div class="col-lg-4 col-md-3 ">
+        <img src="./ressources/images/Plats/plat3.jpeg" class="w-100">
+        <div class="arrondie">
+            
+            <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary">
+                    Voir la recette
+                </button>
+                <button type="button" class="btn btn-sm btn-outline-secondary ml-1">
+                    Favoris
+                </button>
+            </div>
+        </div>
+    </div>
+
