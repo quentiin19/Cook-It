73a2d3d6e2bb4f335ca3c426cb703da1dc73d48e
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-07 19:09:09 +0200

    Update affichagerecettes.php

--- affichagerecettes.php
+++ affichagerecettes.php
@@ -8,7 +8,7 @@
             <div class="card-body arrondie">
                 
 
-                <div class="col-lg-3 col-md-3">
+                <div class="col-lg-3 col-md-3 col-sm-3">
                     <div class="btn-group">
                         <button type="button" class="btn btn-sm btn-outline-secondary">
                             Voir la recette
@@ -18,9 +18,9 @@
                         </button>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-3">
+                <div class="col-lg-6 col-md-6 col-sm-6">
                 </div>
-                <div class="col-lg-3 col-md-3">
+                <div class="col-lg-3 col-md-3 col-sm-3">
                 </div>
             </div>
         </div>
