0bca2176de3b68c0a4a8be04af2fa2256c7862ca
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-10 23:28:04 +0200

    Update ajax.js

--- test/ajax/ajax.js
+++ test/ajax/ajax.js
@@ -10,7 +10,10 @@ var coor_sb = search_bar.getBoundingClientRect();
 //écouteurs
 search_button.addEventListener("click", onclick, false);
 
+
 function clean_str(str){
+    let string = str;
+    console.log(`uncleaned string : ${string}`);
     let a = "àáâäAÀÁÂÄ";
     let e = "èéêëEÈÉÊË";
     let i = "ìíîïIÌÍÎÏ";
@@ -20,32 +23,32 @@ function clean_str(str){
 
     //on supprime les mots de liaisons qui ne sont pas utiliser dans la recherche en bdd
     for (let i = 0; i < liaison.length; i++) {
-        let position = str.indexOf(liaison[i]);
+        let position = string.indexOf(liaison[i]);
         if (position != -1){
-            str.slice(position, liaison[i].length);
+            string.slice(position, liaison[i].length);
         }
     }
 
     //on remplace les lettres qui pourrait fausser notre rechercher en bdd
-    for (let k = 0; k < str.length; k++) {
+    for (let k = 0; k < string.length; k++) {
         for (let j = 0; j < a.length; j++) {
-            if (str[k] == a[j]){
-                str[k] = 'a';
-            }else if(str[k] == e[j]){
-                str[k] = 'e';
-            }else if(str[k] == i[j]){
-                str[k] = 'i';
-            }else if(str[k] == o[j]){
-                str[k] = 'o';
-            }else if(str[k] == u[j]){
-                str[k] = 'u';
-            }else if(str[k] == ' '){
-                str[k] = '-';
+            if (string[k] == a[j]){
+                string[k] = 'a';
+            }else if(string[k] == e[j]){
+                string[k] = 'e';
+            }else if(string[k] == i[j]){
+                string[k] = 'i';
+            }else if(string[k] == o[j]){
+                string[k] = 'o';
+            }else if(string[k] == u[j]){
+                string[k] = 'u';
+            }else if(string[k] == ' '){
+                string[k] = '-';
             }
         }
     }
-    console.log(str);
-    return str;
+    console.log(`cleaned string : ${string}`);
+    return string;
 }
 
 
