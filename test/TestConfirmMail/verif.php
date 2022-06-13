1537be5fdc6e5acc9a90fc58dd663deac1960c58
OussBth <103314967+OussBth@users.noreply.github.com>
2022-06-13 09:26:25 +0200

    Update verif.php

--- test/TestConfirmMail/verif.php
+++ test/TestConfirmMail/verif.php
@@ -8,7 +8,7 @@ if($_GET['id']==$_SESSION['id']){
         $queryPrepared = $pdo->prepare("UPDATE USER set role = 1 WHERE ID=:id ; ");
         $queryPrepared->execute(["id"=>$_SESSION['id']]);
         echo "Vous avez bien validé votre mail, cliquez sur le lien ci dessous pour vous connecter";
-        echo "<br/><a href=http://51.255.172.36/ProjAnn/login.php>Se Connecter</a>";
+        echo "<br/><a href=http://51.255.172.36/login.php>Se Connecter</a>";
         }
     else
         echo "votre lien n'est plus valide";
