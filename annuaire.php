d6eeddaae795294822d60e15370dd9e8f2dccf2a
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-19 10:41:53 +0200

    Update annuaire.php

--- annuaire.php
+++ annuaire.php
@@ -1,2 +1,52 @@
 <?php
 include "template/header.php";
+
+
+
+ 
+if (isConnected()) {
+    $pdo = connectDB();
+
+    $queryPrepared = $pdo->prepare("SELECT * FROM USER");
+    $queryPrepared->execute();
+    $results = $queryPrepared->fetchAll();
+    ?>
+
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Prénom</th>
+                <th>Pseudo</th>
+            </tr>
+        </thead>
+        <tbody>
+            
+            <?php
+            foreach ($results as $user) {
+                echo '  <tr>
+                            <td>'.$user["LASTNAME"].'</td>
+                            <td>'.$user["FIRSTNAME"].'</td>
+                            <td>'.$user["PSEUDO"].'</td>
+                        <tr>
+                            <div class="btn-group">
+                                <a href="delUser.php?id='.$user["ID"].'" class="btn btn-danger">Supprimer</a>
+                                <a href="#" class="btn btn-warning" >Modifier</a>
+                            </div>
+                        </td>
+                    </tr>';
+            }
+            ?>
+
+
+
+
+
+
+
+
+
+
+
+
