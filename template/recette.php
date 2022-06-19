29b33f5ce0dac6213fa04f495bb693f046d7121a
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-06-19 11:18:02 +0200

    Create recette.php

---
+++ template/recette.php
@@ -0,0 +1,19 @@
+<?php include "/templates/header.php" ?>
+
+<div class="col-lg-4 col-md-3 bg-color ">
+                <div class="card mb-4 shadow-sm">
+                    <img src="./ressources/images/entrees.jpeg" class="w-100">
+                    <div class="card-body arrondie">
+                        <div class="row">
+                            <div class="col-lg-3 col-md-3 col-sm-3"></div>
+                                <div class="col-lg-6 col-md-6 col-sm-6 px-2 py-2 border">
+                                    <h4> Titre </h4><br>
+                                    <p>Crée par ******</p>
+                                </div>
+                            <div class="col-lg-3 col-md-3 col-sm-3"></div>
+                        </div>
+                        
+                    
+                    </div>
+                </div>
+            </div>
