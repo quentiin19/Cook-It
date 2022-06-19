53f6eed02a2748227dbf6420e648636f7e0711d0
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-06-19 10:48:00 +0200

    Update AddRecette.php

--- AddRecette.php
+++ AddRecette.php
@@ -31,44 +31,45 @@
 												<h3 class="text-center py-3">Votre Recette </h3>
 												<textarea class="form-control pl-5 my-3" id="textAreaExample1" name="recette_description" rows="20"></textarea>
 											</div>
+											<div class="col-lg-6 col-md-6 col-sm-6">
+												<h3 class="text-center py-3">Ajouter les ingrédients </h3>
+												<?php
+												$pdo = connectDB();
+
+													$queryPrepared = $pdo->prepare("SELECT * FROM INGREDIENTS;");
+													$queryPrepared->execute();
+													$results = $queryPrepared->fetchAll();
+
+													foreach ($results as $key => $ingredient) { 
+														echo '<div class="col-lg-6 col-md-6 col-sm-6 background-body arrondie my-2">
+																	<div class="row align-items-center">
+																			<div class="col-lg-1 col-md-1 col-sm-6">
+																				<input  type="checkbox" name="checkbox'.$ingredient['ID'].'">
+																			</div>
+																			<div class="col-lg-3 col-md-3 col-sm-6">
+																				<img src="'.$ingredient['PICTURE_PATH'].'" height ="70vh" width="70vw"/>
+																			</div>
+																			<div class="col-lg-3 col-md-3 col-sm-3">
+																				<p>'.$ingredient['NAME'].'</p>
+																			</div>
+																			<div class="col-lg-3 col-md-2 col-sm-6 ">
+																				<input class="input-width" type="text" name="quantity'.$ingredient['ID'].'" placeholder="quantité">
+																			</div>
+																			<div class="col-lg-2 col-md-3 col-sm-3">
+																				'.$ingredient['UNIT'].'
+																			</div>		
+																	</div>
+																</div>
+																
+															</div>';
+													}?>
+											</div>
 										</div>
-										<div class="row">
-											<h3 class="text-center py-3">Ajouter les ingrédients </h3>
-										</div>
+										
+		
 
-										<?php
-										$pdo = connectDB();
 
-											$queryPrepared = $pdo->prepare("SELECT * FROM INGREDIENTS;");
-											$queryPrepared->execute();
-											$results = $queryPrepared->fetchAll();
-
-											foreach ($results as $key => $ingredient) { 
-												echo '
-													<div class="row">
-														<div class="col-lg-2 col-md-1 col-sm-0"></div>
-														<div class="col-lg-8 col-md-10 col-sm-12 background-body arrondie my-2">
-															<div class="row align-items-center">
-																	<div class="col-lg-1 col-md-1 col-sm-6">
-																		<input  type="checkbox" name="checkbox'.$ingredient['ID'].'">
-																	</div>
-																	<div class="col-lg-3 col-md-3 col-sm-6">
-																		<img src="'.$ingredient['PICTURE_PATH'].'" height ="70vh" width="70vw"/>
-																	</div>
-																	<div class="col-lg-3 col-md-3 col-sm-3">
-																		<p>'.$ingredient['NAME'].'</p>
-																	</div>
-																	<div class="col-lg-3 col-md-2 col-sm-6 ">
-																		<input class="input-width" type="text" name="quantity'.$ingredient['ID'].'" placeholder="quantité">
-																	</div>
-																	<div class="col-lg-2 col-md-3 col-sm-3">
-																		'.$ingredient['UNIT'].'
-																	</div>		
-															</div>
-														</div>
-														<div class="col-lg-2 col-md-1 col-sm-0"></div>
-													</div>';
-											}?>
+										
 										
 										<div class="row">
 											<div class="col-lg-12 col-md-12">
