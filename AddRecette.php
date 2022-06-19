1253d7f583d43bcd97d77eec1aaf196536ec26e6
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-06-19 10:54:44 +0200

    Update AddRecette.php

--- AddRecette.php
+++ AddRecette.php
@@ -29,7 +29,7 @@
 										<div class="row">
 											<div class="col-lg-6 col-md-6 col-sm-6 input-group">
 												<h3 class="text-center py-3">Votre Recette </h3>
-												<textarea class="form-control pl-5 my-3" id="textAreaExample1" name="recette_description" rows="20"></textarea>
+												<textarea class="form-control ml-5 my-3" id="textAreaExample1" name="recette_description" rows="20"></textarea>
 											</div>
 											<div class="col-lg-6 col-md-6 col-sm-6">
 												<h3 class="text-center py-3">Ajouter les ingrédients </h3>
