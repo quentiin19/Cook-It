ff3f19ca12ca2afd8179af35fc25ffd83834c942
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-19 22:58:25 +0200

    Update api.php

--- ressources/api/api.php
+++ ressources/api/api.php
@@ -34,7 +34,6 @@ class API{
             }
         }
 
-        //création du tableau $result qui sera renvoyer après l'appel de l'api
         //les recettes les plus pertinentes seront trié par index du plus petit au plus grand
         $result = array();
 
@@ -51,6 +50,7 @@ class API{
             $max_pert = 0;
             $max_reci = 0;
 
+            //on cherche l'id avec le plus de pertinence
             foreach ($recipes as $key => $recipe) {
                 if (isset($recipes[$key])) {
                     if ($pertinence[$key] >= $max_pert) {
@@ -72,6 +72,7 @@ class API{
 
         }
 
+        //enfin on met dans le tableau json_recipes les colonnes qui nous intéresse des recettes
         $json_recipes = [];
         foreach ($result as $index => $recipe_id) {
             $queryPrepared = $pdo->prepare("SELECT PSEUDO, TITLE, PICTURE_PATH, RECIPES.ID FROM RECIPES, USER WHERE RECIPES.ID=:id AND RECIPES.ID_CREATOR = USER.ID;");
@@ -84,11 +85,101 @@ class API{
 
         return json_encode($json_recipes);
     }
+
+
+    function ReturnIngredient($key_words){
+        //connexion à la base de données
+        $pdo = connectDB();
+
+        //séparation de tous les mots dans un tableau
+        $array_key_words = explode('-', $key_words);
+
+        //création des tableaux qui vont permettre de gérer la pertinence des recherches
+        $ingredients = array();
+        $pertinence = array();
+
+        //pour chaque mots, on recherche dans la BDD si une recette correspond
+        foreach ($array_key_words as $index=>$word) {
+            $queryPrepared = $pdo->prepare("SELECT ID FROM INGREDIENTS WHERE TITLE LIKE :word;");
+            $queryPrepared->execute(["word"=>"%".$word."%"]);
+            $queryResults = $queryPrepared->fetchAll();
+
+            for ($i = 0; $i < count($queryResults); $i++){ 
+                if (array_search($queryResults[$i][0], $ingredients, false) != false){
+                    $index = array_search($queryResults[$i][0], $ingredients, false);
+                    $pertinence[$index] += 1; 
+                    
+                }else{
+                    //on rajoute la recette dans les recettes qui ressortent de la recherche
+                    array_push($ingredients, $queryResults[$i][0]);
+                    //on initialise la pertinence à 1 car c'est la première itération de la recette dans la recherche
+                    array_push($pertinence, 1);
+                }
+            }
+        }
+
+        //les recettes les plus pertinentes seront trié par index du plus petit au plus grand
+        $result = array();
+
+
+        $is_finished = 0;
+
+        //on insert les recettes une par une dans le tableau $result
+        while (!$is_finished) {
+
+            
+
+            //on définit une variable qui hébergera la pertinence maximum
+            $index_max = 0;
+            $max_pert = 0;
+            $max_reci = 0;
+
+            //on cherche l'id avec le plus de pertinence
+            foreach ($recipes as $key => $recipe) {
+                if (isset($ingredients[$key])) {
+                    if ($pertinence[$key] >= $max_pert) {
+                        $index_max = $key;
+                        $max_pert = $pertinence[$key];
+                        $max_reci = $ingredients[$key];
+                    }
+                }
+            }
+
+            array_push($result, $recipes[$index_max]);
+            unset($recipes[$index_max]);
+            unset($pertinence[$index_max]);
+
+            //si tous les éléments ont été enlevés, le tri est fini
+            if (empty($recipes)){
+                $is_finished = 1;
+            }
+
+        }
+
+        //enfin on met dans le tableau json_recipes les colonnes qui nous intéresse des recettes
+        $json_ingredients = [];
+        foreach ($result as $index => $ingredient_id) {
+            $queryPrepared = $pdo->prepare("SELECT ID, NAME, UNIT, PICTURE_PATH FROM INGREDIENTS WHERE ID=:id;");
+            $queryPrepared->execute(["id"=>$ingredient_id]);
+            $queryResults = $queryPrepared->fetch();
+
+            array_push($json_ingredients, $queryResults);
+        }
+
+
+        return json_encode($json_ingredients);
+    }
 }
 
 $API = new API;
 header('Content-Type: application/json');
-echo $API->ReturnRecipe($_GET['keywords']);
+
+if ($_GET['action'] == 1) {
+    echo $API->ReturnRecipe($_GET['keywords']);
+
+}elseif ($_GET['action'] == 2) {
+    echo $API->ReturnIngredient($_GET['keywords']);
+}
 
 
 ?>
