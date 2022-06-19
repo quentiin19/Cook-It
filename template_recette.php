deb40769a1067290e303517983248d6fc9e87ef8
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-06-19 11:44:20 +0200

    Update template_recette.php

--- template_recette.php
+++ template_recette.php
@@ -12,8 +12,51 @@
                                 </div>
                             <div class="col-lg-3 col-md-3 col-sm-3"></div>
                         </div>
-                        
-                    
                     </div>
                 </div>
-            </div>
+</div>
+<div class="col-lg-4 col-md-3  ">
+                <div class="card mb-4 shadow-sm bg-color py-3 px-3 arrondie" style="width: 35rem ">
+                    <img src="./ressources/images/Desserts/dessert1.jpeg" height="100%" width="100%"> 
+                    <div class="card-body arrondie">
+                        <div class="row">
+                            <div class="col-lg-3 col-md-3 col-sm-3"></div>
+                                <div class="col-lg-6 col-md-6 col-sm-6 px-2 py-2 border">
+                                    <h4> Titre </h4><br>
+                                    <p>Crée par ******</p>
+                                </div>
+                            <div class="col-lg-3 col-md-3 col-sm-3"></div>
+                        </div>
+                    </div>
+                </div>
+</div>
+<div class="col-lg-4 col-md-3  ">
+                <div class="card mb-4 shadow-sm bg-color py-3 px-3 arrondie" style="width: 35rem ">
+                    <img src="./ressources/images/Desserts/dessert1.jpeg" height="100%" width="100%"> 
+                    <div class="card-body arrondie">
+                        <div class="row">
+                            <div class="col-lg-3 col-md-3 col-sm-3"></div>
+                                <div class="col-lg-6 col-md-6 col-sm-6 px-2 py-2 border">
+                                    <h4> Titre </h4><br>
+                                    <p>Crée par ******</p>
+                                </div>
+                            <div class="col-lg-3 col-md-3 col-sm-3"></div>
+                        </div>
+                    </div>
+                </div>
+</div>
+<div class="col-lg-4 col-md-3  ">
+                <div class="card mb-4 shadow-sm bg-color py-3 px-3 arrondie" style="width: 35rem ">
+                    <img src="./ressources/images/Desserts/dessert1.jpeg" height="100%" width="100%"> 
+                    <div class="card-body arrondie">
+                        <div class="row">
+                            <div class="col-lg-3 col-md-3 col-sm-3"></div>
+                                <div class="col-lg-6 col-md-6 col-sm-6 px-2 py-2 border">
+                                    <h4> Titre </h4><br>
+                                    <p>Crée par ******</p>
+                                </div>
+                            <div class="col-lg-3 col-md-3 col-sm-3"></div>
+                        </div>
+                    </div>
+                </div>
+</div>
