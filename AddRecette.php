7411553c93509dbf0ee6d70e6c0e9d77fa29209f
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-06-21 10:30:43 +0200

    Update AddRecette.php

--- AddRecette.php
+++ AddRecette.php
@@ -33,7 +33,7 @@
 											</div>
 											<div class="col-lg-6 col-md-6 col-sm-6">
 												<h3 class="text-center py-3">Ajouter les ingrédients </h3>
-												<div class="overflow-auto" style="height= 400px">
+												<div class="overflow-auto bg-light" style="height= 100px">
 												<?php
 											$pdo = connectDB();
 
