9e922f9ff45b4e0f61afa1611cdf8fe1fbdf2df7
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-21 14:27:20 +0200

    Update ajax.js

--- ressources/js/ajax.js
+++ ressources/js/ajax.js
@@ -140,23 +140,4 @@ function display_results() {
         recettes.appendChild(main_div);
 
     }
-
-
-/*
-<div class="col-lg-3 col-md-4 col-sm-1 py-3">
-    <div class="card mb-4 shadow-sm bg-color py-3 px-3 arrondie" style="width: 35rem ">
-        <img src="./ressources/images/Desserts/dessert1.jpeg" height="100%" width="100%"> 
-        <div class="card-body arrondie">
-            <div class="row">
-                <div class="col-lg-3 col-md-3 col-sm-3"></div>
-                    <div class="col-lg-6 col-md-6 col-sm-6 px-2 py-2 border">
-                        <h4 class="text-center"> Titre </h4><br>
-                        <p class="text-center">Crée par ******</p>
-                    </div>
-                <div class="col-lg-3 col-md-3 col-sm-3"></div>
-            </div>
-        </div>
-    </div>
-</div>
-*/
 }
