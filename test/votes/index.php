46912f7c1d80e919e600490d263ecb1252ce889d
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-26 11:08:41 +0200

    Update index.php

--- test/votes/index.php
+++ test/votes/index.php
@@ -8,9 +8,11 @@ require '../../ressources/fpdf184/fpdf.php';
 <button id="upvote-1">Upvote</button>
 <p id="1"></p>
 <button id="downvote-1">Downvote</button><br>
+<!--
 <button id="upvote-2">Upvote</button>
 <p id="2"></p>
 <button id="downvote-2">Downvote</button><br>
 <button id="upvote-3">Upvote</button>
 <p id="3"></p>
 <button id="downvote-3">Downvote</button><br>
+-->
