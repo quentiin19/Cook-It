0462a8f22014dc97671f311479d22bf9a5aaa82d
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-26 10:56:51 +0200

    api vote

---
+++ test/votes/index.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+require '../../functions.php';
+require '../../ressources/fpdf184/fpdf.php';
+
+?>
+
+<button id="upvote-1">Upvote</button>
+<p id="1"></p>
+<button id="downvote-1">Downvote</button><br>
+<button id="upvote-2">Upvote</button>
+<p id="2"></p>
+<button id="downvote-2">Downvote</button><br>
+<button id="upvote-3">Upvote</button>
+<p id="3"></p>
+<button id="downvote-3">Downvote</button><br>
