54e5c2b124660feba4aa7cece6263ef691055d0c
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-26 14:06:27 +0200

    Update votes.js

--- test/votes/votes.js
+++ test/votes/votes.js
@@ -23,7 +23,7 @@ function vote(number){
     request.addEventListener("load", function(){
         displayVote(id_recipe);
     });
-    request.open("GET", `http://cookit.ovh/test/votes/api_vote?action=2&vote=${number}&user=${id_user.innerText}&token=$${token.innerText}&recipe=${id_recipe}`);
+    request.open("GET", `http://cookit.ovh/test/votes/api_vote.php?action=2&vote=${number}&user=${id_user.innerText}&token=$${token.innerText}&recipe=${id_recipe}`);
     request.send();
 }
 
