f1d9979767150baf8f403c58639d5d46253a99e6
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-06-27 23:17:18 +0200

    Update membres.php

--- membres.php
+++ membres.php
@@ -7,7 +7,7 @@ $afficher_membres = $pdo->prepare("SELECT * FROM USER");
 $afficher_membres -> execute();
 
 foreach ($afficher_membres as $am){
-    echo $am['pseudo'];
+    echo $am['PSEUDO'];
 }
 
 ?>
