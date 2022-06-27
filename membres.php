1f4f5446dbf007f01ff21f028505163b5508a992
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-06-27 23:35:34 +0200

    Update membres.php

--- membres.php
+++ membres.php
@@ -1,13 +1,32 @@
 <?php include "template/header.php";?> 
 
 <?php
+    $pdo = connectDB();
 
-$pdo = connectDB();
-$afficher_membres = $pdo->prepare("SELECT * FROM USER");
-$afficher_membres -> execute();
-
-foreach ($afficher_membres as $am){
-    echo $am['PSEUDO'];
+// Condition  : si la personne est connecté elle ne se verra pas dans les membres ( on verifie si une variable de session existe)
+if(isset($_SESSION['id'])){
+    $afficher_membres = $pdo->prepare("SELECT * FROM USER WHERE id <> ?");
+    $afficher_membres -> execute(array($_SESSION['id']));
+}else{
+    $afficher_membres = $pdo->prepare("SELECT * FROM USER");
+    $afficher_membres -> execute();
 }
+
+?>
+
+<div class="container">
+    <div class="row">
+        <?php
+            foreach ($afficher_membres as $am){
+        ?>
+        <div class="col-lg-3 col-md-4 col-sm-6"> 
+            <div class="bg-light text-center shadow p-3 mb-5 rounded"
+            <?=$am['PSEUDO'] ?> 
+        </div>
+        <?php
+            }
+        ?>
+    </div>
+</div>
 
 ?>
