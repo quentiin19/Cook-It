ea68ec0dae3e72a235e7e43fb750b6a3aa7be541
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-06-28 20:27:40 +0200

    Update UpdateUser.php

--- UpdateUser.php
+++ UpdateUser.php
@@ -11,16 +11,16 @@ if(isConnected() && isAdmin()){
     $results=$queryPrepared->fetch();
 
 
-if(
-	!isset($results["firstname"]) ||
-	!isset($results["lastname"]) || 
-	empty($results["pseudo"]) ||
-	count($results)!=3
-){
+	if(
+		!isset($results["firstname"]) ||
+		!isset($results["lastname"]) || 
+		empty($results["pseudo"]) ||
+		count($results)!=3
+	){
 
-	die("Tentative de Hack ...");
+		die("Tentative de Hack ...");
 
-}
+	}
 
 //récupérer les données du formulaire
 
@@ -32,73 +32,70 @@ $pseudo = $results["pseudo"];
 $queryPrepared = $pdo->prepare("Update USER SET PSEUDO =:pseudo, FIRSTNAME =:firstname, LASTNAME =:lastname WHERE ID =:id");
 $queryPrepared->execute(["pseudo"=> $pseudo, "fistname"=>$firstname, "lastname"=>$lastname, "id"=>$_GET['id'] ]);
 
-}
-
-//Vérification de l'utilisateur
-$id = $_SESSION["id"];
-if(!isConnected()){
+}else if(!isConnected()){				//Vérification de l'utilisateur	
 	die("Il faut se connecter !!!");
 }else{
     $pdo = connectDB();
     $queryPrepared = $pdo->prepare("SELECT * FROM USER WHERE id=:id");
-    $queryPrepared->execute(["id"=>$id]);
+    $queryPrepared->execute(["id"=>$_SESSION["id"]]);
     $results=$queryPrepared->fetch();
+
+
+	if(
+		!isset($_POST["firstname"]) ||
+		!isset($_POST["lastname"]) || 
+		empty($_POST["pseudo"]) ||
+		empty($_POST["oldpassword"])||
+		empty($_POST["password"]) ||
+		empty($_POST["passwordConfirm"]) ||
+		count($_POST)!=6
+	){
+
+		die("Tentative de Hack ...");
+
+	}
+
+	//récupérer les données du formulaire
+
+	$firstname = $_POST["firstname"];
+	$lastname = $_POST["lastname"];
+	$pseudo = $_POST["pseudo"];
+	$oldpwd = $_POST["oldpassword"];
+	$pwd = $_POST["password"];
+	$pwdConfirm = $_POST["passwordConfirm"];
+
+	//vérifier les données
+	$errors = [];
+
+	//Vérification des mots de passes
+	$hasholdpwd= password_hash($oldpwd, PASSWORD_DEFAULT);
+
+	if ($results["HASHPWD"] != $hasholdpwd){
+		$errors[] = "Votre ancien mot de passe n'est pas bon";
+	}
+
+
+	//Mot de passe : Min 8, Maj, Min et chiffre
+	if(strlen($pwd) < 8 ||
+	preg_match("#\d#", $pwd)==0 ||
+	preg_match("#[a-z]#", $pwd)==0 ||
+	preg_match("#[A-Z]#", $pwd)==0 
+	) {
+		$errors[] = "Votre mot de passe doit faire plus de 8 caractères avec une minuscule, une majuscule et un chiffre";
+	}
+	//Confirmation : égalité
+	if( $pwd != $pwdConfirm){
+		$errors[] = "Votre mot de passe de confirmation ne correspond pas";
+	}
+
+
+	//Hashage du nouveau mdp
+	$hashpwd= password_hash($pwd, PASSWORD_DEFAULT);
+
+	//Modification des infos de l'utilisateur dans la BDD
+	$queryPrepared = $pdo->prepare("Update USER SET PSEUDO =:pseudo, HASHPWD =:hashpwd, FIRSTNAME =:firstname, LASTNAME =:lastname WHERE ID =:id");
+	$queryPrepared->execute(["pseudo"=> $pseudo, "hashpwd"=> $hashpwd, "fistname"=>$firstname, "lastname"=>$lastname, "id"=>$id ]);
 }
-
-if(
-	!isset($_POST["firstname"]) ||
-	!isset($_POST["lastname"]) || 
-	empty($_POST["pseudo"]) ||
-    empty($_POST["oldpassword"])||
-	empty($_POST["password"]) ||
-	empty($_POST["passwordConfirm"]) ||
-	count($_POST)!=6
-){
-
-	die("Tentative de Hack ...");
-
-}
-
-//récupérer les données du formulaire
-
-$firstname = $_POST["firstname"];
-$lastname = $_POST["lastname"];
-$pseudo = $_POST["pseudo"];
-$oldpwd = $_POST["oldpassword"];
-$pwd = $_POST["password"];
-$pwdConfirm = $_POST["passwordConfirm"];
-
-//vérifier les données
-$errors = [];
-
-//Vérification des mots de passes
-$hasholdpwd= password_hash($oldpwd, PASSWORD_DEFAULT);
-
-if ($results["HASHPWD"] != $hasholdpwd){
-    $errors[] = "Votre ancien mot de passe n'est pas bon";
-}
-
-
-//Mot de passe : Min 8, Maj, Min et chiffre
-if(strlen($pwd) < 8 ||
-preg_match("#\d#", $pwd)==0 ||
-preg_match("#[a-z]#", $pwd)==0 ||
-preg_match("#[A-Z]#", $pwd)==0 
-) {
-	$errors[] = "Votre mot de passe doit faire plus de 8 caractères avec une minuscule, une majuscule et un chiffre";
-}
-//Confirmation : égalité
-if( $pwd != $pwdConfirm){
-	$errors[] = "Votre mot de passe de confirmation ne correspond pas";
-}
-
-//Hashage du nouveau mdp
-$hashpwd= password_hash($pwd, PASSWORD_DEFAULT);
-
-//Modification des infos de l'utilisateur dans la BDD
-$queryPrepared = $pdo->prepare("Update USER SET PSEUDO =:pseudo, HASHPWD =:hashpwd, FIRSTNAME =:firstname, LASTNAME =:lastname WHERE ID =:id");
-$queryPrepared->execute(["pseudo"=> $pseudo, "hashpwd"=> $hashpwd, "fistname"=>$firstname, "lastname"=>$lastname, "id"=>$id ]);
-
 
 //update des logs
 updateLogs($results["ID"], "modification du profil");
