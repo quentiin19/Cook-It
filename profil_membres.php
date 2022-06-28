75fd445c0afab448f3fd8d1d40f7ccafbddab8ca
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-06-28 12:08:12 +0200

    Update profil_membres.php

--- profil_membres.php
+++ profil_membres.php
@@ -1,77 +1,66 @@
 <?php include "template/header.php";?>
 
-<?php
+<?php	
+$utilisateur_id = $_GET['id'];
 
-$utilisateur_id = ($_GET['id']);
+if (isConnected() && $_GET['id'] != $_SESSION['id']) {
+	$pdo = connectDB();
 
-if (isConnected() && $_GET['id'] <> $_SESSION['id']) {
-			$pdo = connectDB();
+	if(empty($utilisateur_id)){
+		header('Location: /membres.php');
+		exit;
+	}
 
-            if(empty($utilisateur_id)){
-                header('Location: /membres.php');
-                exit;
-            }
+	$queryPrepared = $pdo->prepare("SELECT * FROM USER where ID=:id");
+	$queryPrepared->execute(array($utilisateur_id));
+	$results = $queryPrepared->fetch();
 
-			$queryPrepared = $pdo->prepare("SELECT * FROM USER where ID=:id");
-			$queryPrepared->execute(array($utilisateur_id));
-			$results = $queryPrepared->fetchAll();
-
-            if(!isset($results['id'])){
-                header('Location: /membres.php');
-                exit;
-            }
+	if(!isset($results['id'])){
+		header('Location: /membres.php');
+		exit;
+	}
 ?>
 <div class="row">
 
-			<div class="col-lg-2 col-md-1 col-sm-0"></div>
+	<div class="col-lg-2 col-md-1 col-sm-0"></div>
 
-			<div class="col-lg-8 col-md-10 col-sm-12 h-auto arrondie  ">
-					  <div class="container py-2  h-auto  ">
-					    <div class="row d-flex justify-content-center align-items-center h-100">
-					      
-					        <div class="card bg-color text-white" style="border-radius: 1rem;">
-					          <div class="card-body  text-center">
+	<div class="col-lg-8 col-md-10 col-sm-12 h-auto arrondie  ">
+		<div class="container py-2  h-auto  ">
+			<div class="row d-flex justify-content-center align-items-center h-100">
+					
+				<div class="card bg-color text-white" style="border-radius: 1rem;">
+					<div class="card-body  text-center">
 
-					            <div class="mb-md-5 mt-md-4 pb-5">
-								<div class="row">
-									<div class="col-lg-12 mb-3">
-										<h2> Personnaliser mon avatar </h2>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-lg-12 pb-3" >	
-										<?php //include "avatar/avatar.php"; ?>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-lg-12 pb-3" >	
-										<h2> Mes Informations </h2>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-lg-2 col-md-0 col-sm-0"></div>
-									<div class="col-lg-8 col-md-12 col-sm-12 bg-color arrondie">
-									<!-- Information du Compte -->
-										<form method="POST" action="UpdateUser.php">
-											Votre prénom :<input type="text" class="form-control" name="firstname" placeholder="Votre prénom" value="<?=$results["FIRSTNAME"]?>"><br>
-											Votre nom :<input type="text" class="form-control" name="lastname" placeholder="Votre nom" value=" <?=$results["LASTNAME"]?>"><br>
-											Votre pseudo :<input type="text" class="form-control" name="pseudo" placeholder="Votre pseudo"  required="required" value=" <?=$results["PSEUDO"]?>"><br>
-											<h3 class="py-3"> Modifier mon mot de passe </h3>
-											<input type="password" class="form-control" name="oldpassword" placeholder="ancien mot de passe"  required="required"><br>
-											<input type="password" class="form-control" name="password" placeholder="nouveau mot de passe" required="required"><br>
-											<input type="password" class="form-control" name="passwordConfirm" placeholder="confirmation" required="required"><br>
-											<input  type="submit" class=" ml-3 mt-5 btn btn-light btn-lg py-2 " value="Modifier">
-										</form>
-									</div>
-									<div class="col-lg-2 col-md-0 col-sm-0"></div>
+					<div class="mb-md-5 mt-md-4 pb-5">
+					<div class="row">
+						<div class="col-lg-12 pb-3" >	
+							<h2> Informations du compte </h2>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-lg-2 col-md-0 col-sm-0"></div>
+						<div class="col-lg-8 col-md-12 col-sm-12 bg-color arrondie">
+						<!-- Information du Compte -->
+							<form method="POST" action="UpdateUser.php">
+								Prénom :<input type="text" class="form-control" name="firstname" placeholder="Votre prénom" value="<?=$results["FIRSTNAME"]?>"><br>
+								Nom :<input type="text" class="form-control" name="lastname" placeholder="Votre nom" value=" <?=$results["LASTNAME"]?>"><br>
+								Pseudo :<input type="text" class="form-control" name="pseudo" placeholder="Votre pseudo"  required="required" value=" <?=$results["PSEUDO"]?>"><br>
+								<h3 class="py-3"> Modifier mot de passe </h3>
+								<input type="password" class="form-control" name="oldpassword" placeholder="ancien mot de passe"  required="required"><br>
+								<input type="password" class="form-control" name="password" placeholder="nouveau mot de passe" required="required"><br>
+								<input type="password" class="form-control" name="passwordConfirm" placeholder="confirmation" required="required"><br>
+								<input  type="submit" class=" ml-3 mt-5 btn btn-light btn-lg py-2 " value="Modifier">
+							</form>
+						</div>
+						<div class="col-lg-2 col-md-0 col-sm-0"></div>
 
-					            </div>
-					           </div>
-					        </div>
-					    </div>
-					  </div>
+					</div>
+					</div>
+				</div>
 			</div>
-			<div class="col-lg-2 col-md-1 col-sm-0"></div>
+		</div>
+	</div>
+	<div class="col-lg-2 col-md-1 col-sm-0"></div>
 </div>
 
 
