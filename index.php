2a4635596546aad5fe0482590419f52306db72a1
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-06-28 23:23:30 +0200

    jbc

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include "template/header.php";
 
         <?php 
         $pdo = connectDB();
-        $queryPrepared = $pdo->prepare("SELECT ID_RECIPE, PICTURE_PATH, TITLE, PSEUDO FROM RECIPES, USER WHERE USER.ID = RECIPES.ID_CREATOR ORDER BY RECIPES.ID_RECIPE DESC;");
+        $queryPrepared = $pdo->prepare("SELECT ID_RECIPE, PICTURE_PATH, TITLE, PSEUDO FROM RECIPES ORDER BY RECIPES.ID_RECIPE DESC;");
         $queryPrepared->execute();
         $recipes = $queryPrepared->fetchAll();
 
---
+++ profil.php
@@ -0,0 +1,29 @@
+<?php
+include "template/header.php";
+?>
+        <?php 
+        $pdo = connectDB();
+        $queryPrepared = $pdo->prepare("SELECT ID_RECIPE, PICTURE_PATH, TITLE, PSEUDO FROM RECIPES WHERE  RECIPES.ID_CREATOR = :id ORDER BY RECIPES.ID_RECIPE DESC;");
+        $queryPrepared->execute(["id" => $_SESSION["id"]]);
+        $results = $queryPrepared->fetchAll();
+
+        foreach ($results as $result){
+            echo '<div class="col-lg-3 col-md-4 col-sm-1 py-3">
+                    <div class="card mb-4 shadow-sm bg-color py-3 px-3 arrondie">
+                        <a href="https://cookit.ovh/recette.php?id='.$result['ID_RECIPE'].'">
+                        <img src="'.$result['PICTURE_PATH'].'" class="card-img-top cardh"> </img>
+                        <div class="card-body text-center arrondie">
+                                    <h4>'.$result['TITLE'].'</h4>
+                                    <p>Créé par '.$result['PSEUDO'].'</p>
+                        </div>
+                        </a>        
+                    </div>
+                </div>';
+        }
+
+
+        ?>
+
+         <script src="ressources/js/ajax.js"></script>
+    </body>
+</html>
