40365cdfab22d0ace76b809280d3612037daae08
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-06-28 12:21:50 +0200

    Update profil_membres.php

--- profil_membres.php
+++ profil_membres.php
@@ -31,13 +31,15 @@
 					<div class="row">
 						<div class="col-lg-2 col-md-0 col-sm-0"></div>
 						<div class="col-lg-8 col-md-12 col-sm-12 bg-color arrondie">
+							<div>
+								<img src="<?= $results['PATH_AVATAR']?>"></img>						
+							</div>
 						<!-- Information du Compte -->
 							<form method="POST" action="UpdateUser.php">
 								Prénom :<input type="text" class="form-control" name="firstname" placeholder="Votre prénom" value="<?=$results["FIRSTNAME"]?>"><br>
 								Nom :<input type="text" class="form-control" name="lastname" placeholder="Votre nom" value=" <?=$results["LASTNAME"]?>"><br>
 								Pseudo :<input type="text" class="form-control" name="pseudo" placeholder="Votre pseudo"  required="required" value=" <?=$results["PSEUDO"]?>"><br>
 								<h3 class="py-3"> Modifier mot de passe </h3>
-								<input type="password" class="form-control" name="oldpassword" placeholder="ancien mot de passe"  required="required"><br>
 								<input type="password" class="form-control" name="password" placeholder="nouveau mot de passe" required="required"><br>
 								<input type="password" class="form-control" name="passwordConfirm" placeholder="confirmation" required="required"><br>
 								<input  type="submit" class=" ml-3 mt-5 btn btn-light btn-lg py-2 " value="Modifier">
