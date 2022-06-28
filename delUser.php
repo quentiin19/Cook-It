d0250910d9266bd8e18018fc4e283516aaa0e32c
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-28 11:58:29 +0200

    Update delUser.php

--- delUser.php
+++ delUser.php
@@ -34,63 +34,63 @@ if(isAdmin() || $_GET['id'] == isConnected()){
 
 
 	//suppression des abonnements
-	$queryPrepared = $pdo->prepare("DELETE * FROM SUBSCRIPTION WHERE ID_SUBSCRIPTION=:id OR ID_SUBSCRIBER=:id;");
+	$queryPrepared = $pdo->prepare("DELETE FROM SUBSCRIPTION WHERE ID_SUBSCRIPTION=:id OR ID_SUBSCRIBER=:id;");
 	$queryPrepared->execute(["id"=>$id]);
 
 
 
 	//suppression du frigo
-	$queryPrepared = $pdo->prepare("DELETE * FROM FRIDGE WHERE ID_USER=:id;");
+	$queryPrepared = $pdo->prepare("DELETE FROM FRIDGE WHERE ID_USER=:id;");
 	$queryPrepared->execute(["id"=>$id]);
 
 
 
 	//suppression des recettes que le user à sauvegardé
-	$queryPrepared = $pdo->prepare("DELETE * FROM RECIPES_SAVED WHERE ID_USER=:id;");
+	$queryPrepared = $pdo->prepare("DELETE FROM RECIPES_SAVED WHERE ID_USER=:id;");
 	$queryPrepared->execute(["id"=>$id]);
 
 	//suppression des recettes que le user à créé et que d'autres ont sauvegardé
-	$queryPrepared = $pdo->prepare("DELETE * FROM RECIPES_SAVED WHERE ID_RECIPE IN (SELECT * FROM RECIPES WHERE ID_CREATOR=:id);");
+	$queryPrepared = $pdo->prepare("DELETE FROM RECIPES_SAVED WHERE ID_RECIPE IN (SELECT * FROM RECIPES WHERE ID_CREATOR=:id);");
 	$queryPrepared->execute(["id"=>$id]);
 
 
 
 	//suppression des images dans les recettes que le user à créé
-	$queryPrepared = $pdo->prepare("DELETE * FROM PICTURE WHERE ID_RECIPE IN (SELECT * FROM RECIPES WHERE ID_CREATOR=:id);");
+	$queryPrepared = $pdo->prepare("DELETE FROM PICTURE WHERE ID_RECIPE IN (SELECT * FROM RECIPES WHERE ID_CREATOR=:id);");
 	$queryPrepared->execute(["id"=>$id]);
 
 
 
 	//suppression des besoin des recettes créé par le user
-	$queryPrepared = $pdo->prepare("DELETE * FROM NEED WHERE ID_RECIPE IN (SELECT * FROM RECIPES WHERE ID_CREATOR=:id);");
+	$queryPrepared = $pdo->prepare("DELETE FROM NEED WHERE ID_RECIPE IN (SELECT * FROM RECIPES WHERE ID_CREATOR=:id);");
 	$queryPrepared->execute(["id"=>$id]);
 
 
 
 	//suppression des votes du user
-	$queryPrepared = $pdo->prepare("DELETE * FROM VOTES WHERE ID_USER=:id;");
+	$queryPrepared = $pdo->prepare("DELETE FROM VOTES WHERE ID_USER=:id;");
 	$queryPrepared->execute(["id"=>$id]);
 
 	//suppression des votes sur les recettes créé par le user
-	$queryPrepared = $pdo->prepare("DELETE * FROM VOTES WHERE ID_RECIPE IN (SELECT * FROM RECIPES WHERE ID_CREATOR=:id);");
+	$queryPrepared = $pdo->prepare("DELETE FROM VOTES WHERE ID_RECIPE IN (SELECT * FROM RECIPES WHERE ID_CREATOR=:id);");
 	$queryPrepared->execute(["id"=>$id]);
 
 
 
 	//suppression des recettes créées par le user
-	$queryPrepared = $pdo->prepare("DELETE * FROM RECIPES WHERE ID_CREATOR=:id;");
+	$queryPrepared = $pdo->prepare("DELETE FROM RECIPES WHERE ID_CREATOR=:id;");
 	$queryPrepared->execute(["id"=>$id]);
 
 
 
 	//suppression des logs du user
-	$queryPrepared = $pdo->prepare("DELETE * FROM LOGS WHERE ID=:id;");
+	$queryPrepared = $pdo->prepare("DELETE FROM LOGS WHERE ID=:id;");
 	$queryPrepared->execute(["id"=>$id]);
 
 
 
 	//suppression du user lui-même
-	$queryPrepared = $pdo->prepare("DELETE * FROM USER WHERE ID=:id;");
+	$queryPrepared = $pdo->prepare("DELETE FROM USER WHERE ID=:id;");
 	$queryPrepared->execute(["id"=>$id]);
 
 
