e4e08287777d3174e00b34d0622bb204b398f3e6
OussBth <103314967+OussBth@users.noreply.github.com>
2022-06-28 23:00:35 +0200

    Update verif.php

--- test/TestConfirmMail/verif.php
+++ test/TestConfirmMail/verif.php
@@ -2,9 +2,11 @@
 session_start();
 require "../../functions.php";
 //une fois qu'on se co on met à jour la bdd de
-$pdo = connectDB();
+print_r($_SESSION);
+print_r($_GET);
 if($_GET['id']==$_SESSION['id']){
     if($_GET['cle']== $_SESSION['cle']){
+        $pdo = connectDB();
         $queryPrepared = $pdo->prepare("UPDATE USER set role = 1 WHERE ID=:id ; ");
         $queryPrepared->execute(["id"=>$_SESSION['id']]);
         echo "Vous avez bien validé votre mail, cliquez sur le lien ci dessous pour vous connecter";
