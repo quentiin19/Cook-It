4d90f1f14639c0acc52a520b20e6dcb8807fd56b
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-29 10:58:39 +0200

    Update index.php

--- test/previsu/index.php
+++ test/previsu/index.php
@@ -1,3 +1,9 @@
 <form method='GET' action=''>
-    <input type='file'>
+    <input type='file' name='fichier'>
+    <?php
+    print_r($_FILES);
+    if(isset($_FILES)){
+        echo '<img src="';
+    }
+    ?>
 </form>
