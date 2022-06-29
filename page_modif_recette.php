0c9f2b2cfcf612764a2c3a72202c6e7bcd33cb79
OussBth <103314967+OussBth@users.noreply.github.com>
2022-06-29 21:31:44 +0200

    Update page_modif_recette.php

--- page_modif_recette.php
+++ page_modif_recette.php
@@ -68,9 +68,10 @@ $resultN = $queryPrepared->fetchAll();
 										}
 										?>
 									</table>
-								</div>																
 								</div>
-								<input  type="submit" class=" ml-3 mt-5 btn btn-light btn-lg py-2 " value="Modifier">	
+								<input  type="submit" class=" ml-3 mt-5 btn btn-light btn-lg py-2 " value="Modifier">
+								</div>
+								
 							</form>
 						</div>
 						<div class="col-lg-2 col-md-0 col-sm-0"></div>
