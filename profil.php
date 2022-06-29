325d0fd4d0b5a2ec816058527bf06045f0783d4b
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-06-29 09:41:10 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -56,15 +56,16 @@ include "template/header.php";
         $results = $queryPrepared->fetchAll();
 
         foreach ($results as $result){
-            echo '<div class="col-lg-4 col-md-4 col-sm-1 py-3">
-                    <div class="card mb-4 shadow-sm bg-color py-3 px-3 arrondie">
-                        <a href="https://cookit.ovh/recette.php?id='.$result['ID_RECIPE'].'">
-                        <img src="'.$result['PICTURE_PATH'].'" class="card-img-top cardh"> </img>
-                        <div class="card-body text-center arrondie">
-                                    <h4>'.$result['TITLE'].'</h4>
-                                    
+            echo '<div class="d-flex justify-content-center">
+                    <div class="col-lg-4 col-md-4 col-sm-1 py-3">
+                        <div class="card mb-4 shadow-sm bg-color py-3 px-3 arrondie">
+                            <a href="https://cookit.ovh/recette.php?id='.$result['ID_RECIPE'].'">
+                            <img src="'.$result['PICTURE_PATH'].'" class="card-img-top cardh"> </img>
+                            <div class="card-body text-center arrondie">
+                                        <h4>'.$result['TITLE'].'</h4>   
+                            </div>
+                            </a>        
                         </div>
-                        </a>        
                     </div>
                 </div>';
         }
