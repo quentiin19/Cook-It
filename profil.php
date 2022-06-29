9b7cc19f0eba0ffb96fadac7e44a51f05bd1aa8d
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-29 10:40:54 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -23,7 +23,7 @@ include "template/header.php";
         <div class="row">
             <div class="container bg-color justify-content-center my-3 py-5">
                 <div class="col-lg-5">
-                    <img src=".<?= $us[7]?>" class="text-right" alt="avatar">
+                    <img src=".<?= $us['PATH_AVATAR']?>" class="text-right" alt="avatar">
                 </div>
                 <div class="col-lg-7">
                     <div class="row">
