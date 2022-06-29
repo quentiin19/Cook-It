5f81d8a3879542aba6f73aa995e104ada7b4da2b
OussBth <103314967+OussBth@users.noreply.github.com>
2022-06-29 20:47:51 +0200

    Update page_modif_recette.php

--- page_modif_recette.php
+++ page_modif_recette.php
@@ -6,13 +6,13 @@
 if (isConnected() == $_SESSION['id']){
 $pdo = connectDB();
 
-$queryPrepared = $pdo->prepare("SELECT *  FROM RECIPES WHERE ID_CREATOR = :id  AND ID_RECIPE = :idr;");
-$queryPrepared ->execute(["id" => $_SESSION['id'], "idr" => $_GET['id']]);
+$queryPrepared = $pdo->prepare("SELECT *  FROM RECIPES WHERE ID_RECIPE = :idr;");
+$queryPrepared->execute(["idr" => $_GET['id']]);
 echo "1";
 $resultR = $queryPrepared->fetch();
 
 $queryPrepared = $pdo->prepare("SELECT *  FROM NEED WHERE ID_RECIPE = :id;");
-$queryPrepared ->execute(["id" => $_GET['id']]);
+$queryPrepared->execute(["id" => $_GET['id']]);
 $resultN = $queryPrepared->fetch();
 }
 
