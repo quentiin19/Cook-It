9198985387417bbd55849bf555f0de47c1282790
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-29 11:24:20 +0200

    Update index.php

--- test/votes/index.php
+++ test/votes/index.php
@@ -2,6 +2,7 @@
 session_start();
 require '../../functions.php';
 require '../../ressources/fpdf184/fpdf.php';
+include '../../template/header.php';
 
 echo '<p id="user_id" hidden="hidden">'.$_SESSION['id'].'</p>';
 echo '<p id="user_token" hidden="hidden">'.$_SESSION['token'].'</p>';
