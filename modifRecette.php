f3310ad8601c9215bd609be17f67e06f35ffb5c2
OussBth <103314967+OussBth@users.noreply.github.com>
2022-06-30 11:40:30 +0200

    Update modifRecette.php

--- modifRecette.php
+++ modifRecette.php
@@ -7,7 +7,7 @@ echo '<pre>';
 print_r($_SESSION);
 echo'</pre>';
 echo '<pre>';
-print_r($_GET);
+print_r($_POST['idrecipe']);
 echo'</pre>';
 
 if (isConnected() == $_SESSION['id']) {
@@ -15,15 +15,15 @@ if (isConnected() == $_SESSION['id']) {
 		
 		
 		$queryPrepared = $pdo->prepare("SELECT *  FROM RECIPES WHERE ID_CREATOR = :id  AND ID_RECIPE = :idr");
-		$queryPrepared ->execute(["id" => $_SESSION["id"], "idr" => $_GET["id"]]);
+		$queryPrepared ->execute(["id" => $_SESSION["id"], "idr" => $_POST["idrecipe"]]);
 		$resultR = $queryPrepared->fetch();
 		
 		$queryPrepared = $pdo->prepare("SELECT *  FROM NEED WHERE ID_RECIPE = :id");
-		$queryPrepared ->execute(["id" => $_GET["id"]]);
+		$queryPrepared ->execute(["id" => $_POST["idrecipe"]]);
 		$resultN = $queryPrepared->fetchAll();
 		
 		$queryPrepared = $pdo->prepare("SELECT * FROM INGREDIENTS WHERE ID IN (SELECT ID_INGREDIENT FROM NEED WHERE ID_RECIPE = :id);");
-		$queryPrepared->execute(["id"=>$_GET['id']]);
+		$queryPrepared->execute(["id"=>$_POST["idrecipe"]]);
 		$ingredients = $queryPrepared->fetchAll();
 		
 		echo '<pre>';
