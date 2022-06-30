7d6174fedf7cb48f006684567b5e0fd9e04aa104
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-06-30 23:10:24 +0200

    viex

--- viewsub.php
+++ viewsub.php
@@ -1,9 +1,11 @@
 <?php include "template/header.php";?> 
 
+
 <?php
     $pdo = connectDB();
 
 // Condition  : si la personne est connecté elle ne se verra pas dans les membres ( on verifie si une variable de SESSIO$_SESSION existe)
+if($_GET['display']==1){
 
     $queryPrepared = $pdo->prepare("SELECT * FROM USER WHERE ID IN (SELECT ID_SUBSCRIPTION FROM SUBSCRIPTION WHERE ID_SUBSCRIBER = :id) ORDER BY PSEUDO ASC;");
     $queryPrepared -> execute(['id' =>$_GET['id']]);
@@ -31,8 +33,44 @@
         </div>
         <?php
             }
+    echo'    
+    </div>
+</div>';
+}elseif($_GET['display']==2){
+?>
+
+<?php
+// Condition  : si la personne est connecté elle ne se verra pas dans les membres ( on verifie si une variable de SESSIO$_SESSION existe)
+
+    $queryPrepared = $pdo->prepare("SELECT * FROM USER WHERE ID IN (SELECT ID_SUBSCRIPTION FROM SUBSCRIPTION WHERE ID_SUBSCRIPTION = :id) ORDER BY PSEUDO ASC;");
+    $queryPrepared -> execute(['id' =>$_GET['id']]);
+    $abonnement = $queryPrepared -> fetchAll();
+
+
+?>
+<h1 class="text-center my-3">abonner</h1>
+<div class="container py-5">
+    <div class="row">
+        <?php
+            foreach ($abonnement as $ab){
+        ?>
+        <div class="col-lg-3 col-md-4 col-sm-6"> 
+            <div class=" card bg-color text-center shadow p-3 mb-5 rounded">
+                <div>
+                    <img src="<?= $ab['PATH_AVATAR']?>" class="card-img-top cardh my-3"></img>
+                </div>
+                <div>
+                    <?= $ab['PSEUDO'] ?>  
+                </div>
+                <a href="https://cookit.ovh/profil_membres.php?id=<?= $ab['ID'] ?>" class ="bg-light rounded my-3"> Voir le profil</a>
+                
+            </div>
+        </div>
+        <?php
+            }
         ?>
     </div>
 </div>
-
+<?php
+}
 ?>
