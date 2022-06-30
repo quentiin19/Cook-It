5fe5c2d495e57ab794df981885a6dc41ad254553
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-30 12:09:55 +0200

    Update messagerie.php

--- test/messagerie/messagerie.php
+++ test/messagerie/messagerie.php
@@ -74,7 +74,7 @@ if($state1[0] == 1 && $state2[0] == 1){
                     <!-- 2eme personne -->
                     <div class="d-flex flex-row justify-content-end mb-4 pt-1">
                         <div>
-                            <p class="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">Hiii, I'm good.</p>00:05
+                            <span><p class="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">Hiii, I'm good.</p>00:05</span>
                             <p class="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">How are you doing?</p>
                             <p class="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">Long time no see! Tomorrow office. will be free on sunday.</p>
                             <p class="small me-3 mb-3 rounded-3 text-muted d-flex justify-content-end">00:06</p>
