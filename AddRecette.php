ab2382673f07afb2cf5e1fe0699c2b8620b36c79
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-06-30 10:33:15 +0200

    Update AddRecette.php

--- AddRecette.php
+++ AddRecette.php
@@ -6,8 +6,7 @@
 
 
 <div class="row">
-    <div class="col-lg-2 col-md-1 col-sm-0"></div>
-    <div class="col-lg-8 col-md-10 col-sm-12 h-auto arrondie  ">
+    <div class="col-lg-12 col-md-12 col-sm-12 h-auto arrondie  ">
         <div class="container py-2  h-auto  ">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="card bg-color text-white" style="border-radius: 1rem;">
@@ -92,7 +91,6 @@
             </div>
         </div>
     </div>
-    <div class="col-lg-2 col-md-1 col-sm-0"></div>
 </div>
 
 <?php
