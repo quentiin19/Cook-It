609d4b20f2136dc81fc4ce7e883e93b9947914b7
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-30 12:10:59 +0200

    Update messagerie.php

--- test/messagerie/messagerie.php
+++ test/messagerie/messagerie.php
@@ -76,7 +76,7 @@ if($state1[0] == 1 && $state2[0] == 1){
                         <div>
                             <p class="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">Hiii, I'm good.</p>
                             <p class="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">How are you doing?</p>
-                            <p class="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">Long time no see! Tomorrow ofdddddddddddddddfdfdfdfkdfdlfkldkfldfkldkfldklfdkfldkdflfice. will be free on sunday.</p>
+                            <p class="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">Long time no see! Tomorrow ofdddddddddddddddfdfdsfkslfklmdskflmskflmdskflmskflmskflmskfmlskmlfksmlfkldmskfmlsfdfdfkdfdlfkldkfldfkldkfldklfdkfldkdflfice. will be free on sunday.</p>
                             <p class="small me-3 mb-3 rounded-3 text-muted d-flex justify-content-end">00:06</p>
                         </div>
                         <!-- <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava4-bg.webp" alt="avatar 1" style="width: 45px; height: 100%;">-->
