a4e5359cb4d85a0b5a5e64a1451b4e8d8db9bf37
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-30 11:49:04 +0200

    Update messagerie.php

--- test/messagerie/messagerie.php
+++ test/messagerie/messagerie.php
@@ -1,5 +1,7 @@
 <?php
-include '../../template/header.php';
+session_start();
+include '../../functions.php';
+
 
 
 $pdo = connectDB();
@@ -24,8 +26,17 @@ if($state1[0] == 1 && $state2[0] == 1){
     $queryPrepared->execute(['id'=>$_GET['id']]);
     $friendName = $queryPrepared->fetch();
     
-    ?>
-    
+?>
+
+
+<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet"  href="./ressources/css/style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     
     
     
