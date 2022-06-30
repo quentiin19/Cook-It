7ad1c51d8f387555836cdefc3b7805fa7f1036bb
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-06-30 13:40:59 +0200

    Update preview-image.js

--- preview-image.js
+++ preview-image.js
@@ -1,7 +1,7 @@
 const inpFile = document.getElementById("inpFile");
 const previewContainer = document.getElementById("imagePreview");
 const previewImage = previewContainer.querySelector(".image-preview__image");
-const previewDefaultText = previewContainer.querySelector(".image-preview__image");
+const previewDefaultText = previewContainer.querySelector(".text-preview");
 
 inpFile.addEventListener("change", function() {     
     const file = this.files[0];
