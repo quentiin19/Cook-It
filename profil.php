4e2eef6e2e1ab9e78f9f192adfc3003cbf2b9e48
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-06-30 23:54:15 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -141,10 +141,10 @@ include "template/header.php";
                             <h4>Recettes : <?= $nbrecipe[0]?></h4>
                         </div>
                         <div class="col-lg-4">
-                            <a href="<?= 'https://cookit.ovh/viewsub.php?id='.$_GET['id'].'&display=1'?>"><h4>Abonnés : <?= $abonnes[0]?></h4><a>
+                            <a href="<?= 'https://cookit.ovh/viewsub.php?id='.$_GET['id'].'&display=2'?>"><h4>Abonnés : <?= $abonnes[0]?></h4><a>
                         </div>
                         <div class="col-lg-4">
-                            <a href="<?= 'https://cookit.ovh/viewsub.php?id='.$_GET['id'].'&display=2'?>"><h4>Abonnement : <?= $abonnement[0]?></h4><a>
+                            <a href="<?= 'https://cookit.ovh/viewsub.php?id='.$_GET['id'].'&display=1'?>"><h4>Abonnement : <?= $abonnement[0]?></h4><a>
                             
                         </div>
                     </div>
