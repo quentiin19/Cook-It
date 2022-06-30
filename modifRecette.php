71aa2ec051b0a298dfa1b7b747cb99590b8d77e3
OussBth <103314967+OussBth@users.noreply.github.com>
2022-06-30 23:06:22 +0200

    Update modifRecette.php

--- modifRecette.php
+++ modifRecette.php
@@ -88,15 +88,21 @@ if (isConnected() == $_SESSION['id'] || isAdmin()) {
 				}
 			
 			}
-			//on inscrit le chemin de la nouvelle image dans la recette
-			$queryPrepared = $pdo->prepare("UPDATE RECIPES set PICTURE_PATH = :imgp where ID_RECIPE= :id;");
-			$queryPrepared->execute(["imgp"=> "https://cookit.ovh/ressources/images/images-recettes/".$final_file_name, "id"=>$_POST["idrecipe"]]);
 
-			echo $final_file_name;
-			echo '<pre>';
-			print_r($final_file_name);
-			echo '</pre>';
+
+
 		}
+		
+		
+		
+		echo $final_file_name;
+		echo '<pre>';
+		print_r($final_file_name);
+		echo '</pre>';
+		
+		//on inscrit le chemin de la nouvelle image dans la recette
+		$queryPrepared = $pdo->prepare("UPDATE RECIPES set PICTURE_PATH = :imgp where ID_RECIPE= :id;");
+		$queryPrepared->execute(["imgp"=> "https://cookit.ovh/ressources/images/images-recettes/".$final_file_name, "id"=>$_POST["idrecipe"]]);
 		
 		//on rentre le titre
 		$queryPrepared = $pdo->prepare("UPDATE RECIPES set TITLE = :title WHERE ID_RECIPE = :id");
