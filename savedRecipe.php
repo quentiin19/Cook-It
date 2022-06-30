07e6b9cd9066ccc68d5adc16a377e994cdd8738c
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-30 18:49:38 +0200

    Update savedRecipe.php

--- savedRecipe.php
+++ savedRecipe.php
@@ -3,7 +3,7 @@ include "template/header.php";
 
 
 $pdo = connectDB();
-$queryPrepared = $pdo->prepare("SELECT * FROM RECIPES WHERE ID_RECIPE IN (SELECT ID_RECIPE FROM RECIPES_SAVED WHERE ID_USER = :id ORDER BY DATE_SAVED ASC);");
+$queryPrepared = $pdo->prepare("SELECT * from RECIPES_SAVED,RECIPES where RECIPES.ID_RECIPE = RECIPES_SAVED.ID_RECIPE order by RECIPES_SAVED.DATE_SAVED");
 $queryPrepared->execute(['id'=>$_SESSION['id']]);
 $recipes = $queryPrepared->fetchAll();
 
