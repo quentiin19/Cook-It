7ee0861d8bc59427ac5ff37417a15876d4e0c61a
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-06-30 18:23:59 +0200

    Update saveRecipe.php

--- saveRecipe.php
+++ saveRecipe.php
@@ -18,7 +18,7 @@ if(isConnected()){
         $queryPrepared = $pdo->prepare("DELETE FROM RECIPES_SAVED WHERE ID_RECIPE = :id_recipe AND ID_USER = :id_user;");
         $queryPrepared->execute(["id_recipe"=>$id_recipe, "id_user"=>$id_user]);
     }else{
-        $queryPrepared = $pdo->prepare("INSERT INTO RECIPES_SAVED VALUES (:id_user, :id_recipe, CURRENT_TIMESTAMP);");
+        $queryPrepared = $pdo->prepare("INSERT INTO RECIPES_SAVED VALUES (:id_user, :id_recipe, NOW());");
         $queryPrepared->execute(["id_recipe"=>$id_recipe, "id_user"=>$id_user]);
     }
 }
