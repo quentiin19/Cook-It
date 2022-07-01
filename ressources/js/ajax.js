aed6a115f2edc0e814a221773fdec12a38b62538
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-07-01 19:19:50 +0200

    Update ajax.js

--- ressources/js/ajax.js
+++ ressources/js/ajax.js
@@ -72,7 +72,7 @@ function display_results_ingredient() {
     console.log(JSON.parse(request_ajax.response));
 
     let ingredients_resp = JSON.parse(request_ajax.response);
-    console.log(ingredients_resp.children[0]);
+    console.log(ingredients_resp.children);
 
     //hide tous les éléments
     for (const element of ingredients_resp.childNodes) {
