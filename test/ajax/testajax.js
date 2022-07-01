e33a5c17b46d84adef02941fc0b5aab4278ce2ed
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-07-01 18:17:20 +0200

    Update testajax.js

--- test/ajax/testajax.js
+++ test/ajax/testajax.js
@@ -3,5 +3,7 @@ const div = document.getElementById("recettes");
 console.log("linked");
 
 for (const child of div.childNodes) {
-    console.log(child);
+    if((child.innerText % 2) == 0){
+        child.hidden = true;
+    }
 }
