5efef4e7a049aea11ffc571546fa6c5ca35df80f
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-07-01 18:47:02 +0200

    Update ajax.js

--- ressources/js/ajax.js
+++ ressources/js/ajax.js
@@ -41,6 +41,7 @@ function clean_str_ajax(str){
 }
 
 function onclickingredients() {
+    console.log("maj");
     //récupération de la valeur dans la barre de recherche
     var keywords = clean_str_ajax(search_bar_recipes.value);
 
