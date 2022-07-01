014e4d2893be43c4c717a2117d187a1c8165e9c6
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-07-01 16:15:10 +0200

    Update profil2.0.php

--- profil2.0.php
+++ profil2.0.php
@@ -3,8 +3,14 @@
 include 'template/header.php';
 ?>
 
-<div class="row">
-    <div class="col-lg-2 col-md-2 col-sm-2 bg-color ">
+<div class="row bg-danger">
+
+
+
+
+
+</div>
+    <!-- <div class="col-lg-2 col-md-2 col-sm-2 bg-color ">
         <div class=" d-flex align-items-center justify-content-center ">
             <div class="">
                 <a href="#" >Modifier mon profil</a>
@@ -21,7 +27,7 @@ include 'template/header.php';
         </div>
     </div>
     <div class="col-lg-10 col-md-10 col-sm-10"></div>
-</div>
+</div> -->
 
     
         
