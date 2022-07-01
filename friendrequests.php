ee9daa6aa54175cc5d32f09d07882f8c26d8ac82
OussBth <103314967+OussBth@users.noreply.github.com>
2022-07-01 18:37:00 +0200

    Update friendrequests.php

--- friendrequests.php
+++ friendrequests.php
@@ -4,8 +4,8 @@ include "template/header.php";
 
 if (isConnected() == $_SESSION['id']){
 	$pdo = connectDB();
-	$queryPrepared = $pdo->prepare("SELECT *  FROM RECIPES WHERE ID_SUBSCRIBER = :idsber AND STATUS = 1;");
-	$queryPrepared->execute(["idsber" => $_GET['id']]);
+	$queryPrepared = $pdo->prepare("SELECT *  FROM RECIPES WHERE ID_SUBSCRIPTION = :idstion AND STATUS = 0;");
+	$queryPrepared->execute(["idstion" => $_GET['id']]);
 	$friends = $queryPrepared->fetch();
 
 }
@@ -18,7 +18,7 @@ foreach ($friends as $friend){
     echo '
             <div class="col-lg-4 col-md-4 col-sm-1 py-3">
                 <div class="card mb-4 shadow-sm bg-color py-3 px-3 arrondie">
-                    <a href="https://cookit.ovh/profil.php?id='.$friend['ID_SUBSCRIPTION'].'"></a>        
+                    <a href="https://cookit.ovh/profil.php?id='.$friend['ID_SUBSCRIBER'].'"></a>        
                 </div>
             </div>';
 }
