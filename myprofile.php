92dd226c892e0ad84c4bcdbeabb99d96527ef7d3
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-07-01 18:54:03 +0200

    Update myprofile.php

--- myprofile.php
+++ myprofile.php
@@ -5,7 +5,7 @@ include 'template/header.php';
 
 
 <div class="row" height = "100%" >
-    <div class="col-lg-2 col-md-2 col-sm-2 bg-color my-3 ml-5 arrondie ">
+    <div class="col-lg-2 col-md-2 col-sm-2 bg-color mx-3 my-3 ml-5 arrondie ">
             <div class="row">
                 <div class="col-lg-12 my-5 py-2 pl-2">
                     <a class="text-white" href="https://cookit.ovh/modif_profil.php?id=<?= $_SESSION['id']?>" >Modifier mon profil</a>
