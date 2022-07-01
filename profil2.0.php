980945c6b4e4843f2fe75e98f1c0e0b6a405bf26
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-07-01 16:54:35 +0200

    Update profil2.0.php

--- profil2.0.php
+++ profil2.0.php
@@ -22,7 +22,7 @@ include 'template/header.php';
                 </div>
             </div>
             <div class="row">
-                <div class="col-lg-12 my-5 py-2 pl-2">
+                <div class="col-lg-12 my-4 py-2 pl-2">
                     <a href="#" >Modifier mon mot de passe</a>
                 </div>
             </div>
