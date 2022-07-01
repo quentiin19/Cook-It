a57ed3458d365f6ccfcfc6fe5e91516dda28e2e1
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-07-01 18:13:53 +0200

    ajax

--- test/ajax/index.php
+++ test/ajax/index.php
@@ -1,3 +1,12 @@
-<input type="text" id="search_bar_ajax">
-<div id="recettes"></div>
-<script src="ajax.js"></script>
+<div id="recettes">
+    <p>1</p>
+    <p>2</p>
+    <p>3</p>
+    <p>4</p>
+    <p>5</p>
+    <p>6</p>
+    <p>7</p>
+    <p>8</p>
+    <p>9</p>
+<div>
+<script src="testajax.js"></script>
---
+++ test/ajax/testajax.js
@@ -0,0 +1,7 @@
+const div = document.getElementById("recettes");
+
+console.log("linked");
+
+for (const child of div.children) {
+    console.log(child);
+}
