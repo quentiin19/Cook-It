8451828904fef8a8af03e703c6d886da3f5835bf
OussBth <103314967+OussBth@users.noreply.github.com>
2022-07-01 12:33:52 +0200

    Update modif_profil.php

--- modif_profil.php
+++ modif_profil.php
@@ -34,7 +34,7 @@ if (isConnected()) {
 											Votre prénom :<input type="text" class="form-control" name="firstname" placeholder="Votre prénom" value="<?=$results["FIRSTNAME"]?>"><br>
 											Votre nom :<input type="text" class="form-control" name="lastname" placeholder="Votre nom" value=" <?=$results["LASTNAME"]?>"><br>
 											Votre pseudo :<input type="text" class="form-control" name="pseudo" placeholder="Votre pseudo"  required="required" value=" <?=$results["PSEUDO"]?>"><br>
-											Votre Descriptin :<textarea row=3 type="text" class="form-control" name="description" placeholder="Votre description"  required="required" value=" <?=$results["DESCRIPTION_PROFIL"]?>"><br>
+											Votre Descriptin :<textarea row=3 type="text" class="form-control" name="description" placeholder="Votre description"  required="required" value=" <?=$results["DESCRIPTION_PROFIL"]?>"></textarea><br>
 											<h3 class="py-3"> Confirmez en rentrant votre mot de passe </h3>
 											<input type="password" class="form-control" name="password" placeholder="mot de passe"  required="required"><br>
 											<?php
