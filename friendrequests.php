034dc61bd3089e36cd04c700807f4e61ad2b00da
OussBth <103314967+OussBth@users.noreply.github.com>
2022-07-01 18:59:31 +0200

    Update friendrequests.php

--- friendrequests.php
+++ friendrequests.php
@@ -13,22 +13,22 @@ echo'<pre>';
 print_r($friends);
 echo'</pre>';
 
-// echo'
-// <div class="row">
-//     <div class="col-lg-2"></div>
-//     <div class="col-lg-8">';
-// foreach ($friends as $friend){
-//     echo '
-//             <div class="col-lg-4 col-md-4 col-sm-1 py-3">
-//                 <div class="card mb-4 shadow-sm bg-color py-3 px-3 arrondie">
-//                     <a href="https://cookit.ovh/profil.php?id='.$friend['ID_SUBSCRIBER'].'"></a>        
-//                 </div>
-//             </div>';
-// }
-// echo '
-//     </div>
-//     <div class="col-lg-2"></div>
-// </div>';
+echo'
+<div class="row">
+    <div class="col-lg-2"></div>
+    <div class="col-lg-8">';
+foreach ($friends as $friend){
+    echo '
+            <div class="col-lg-4 col-md-4 col-sm-1 py-3">
+                <div class="card mb-4 shadow-sm bg-color py-3 px-3 arrondie">
+                    <a href="https://cookit.ovh/profil.php?id='.$friend[0].'"></a>        
+                </div>
+            </div>';
+}
+echo '
+    </div>
+    <div class="col-lg-2"></div>
+</div>';
 
 
 ?>
