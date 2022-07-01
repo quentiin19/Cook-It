3da37a3572b6a1c70141d810b6510da443f409f9
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-07-01 16:27:19 +0200

    Update profil2.0.php

--- profil2.0.php
+++ profil2.0.php
@@ -5,21 +5,7 @@ include 'template/header.php';
 
 <div class="row" height = "100%" >
     <div class="col-lg-2 col-md-2 col-sm-2 bg-danger ">
-        Blelalleno
-    </div>
-    <div class="col-lg-10 col-md-10 col-sm-10 bg-light">
-        <p>bzandknzenf</p>
-        <p>bzandknzenf</p>
-        aaaa
-    </div>  
-
-
-
-
-
-</div>
-    <!-- <div class="col-lg-2 col-md-2 col-sm-2 bg-color ">
-        <div class=" d-flex align-items-center justify-content-center ">
+        <div class=" d-flex  justify-content-center ">
             <div class="">
                 <a href="#" >Modifier mon profil</a>
             </div>
@@ -34,10 +20,111 @@ include 'template/header.php';
             </div>
         </div>
     </div>
-    <div class="col-lg-10 col-md-10 col-sm-10"></div>
-</div> -->
+    <div class="col-lg-10 col-md-10 col-sm-10 bg-light">
+    <div>
+        <input type="text" id="search-bar-recipe" placeholder="rechercher" class="text-center">
+        <div id="recettes">
 
-    
+        <?php 
+        $pdo = connectDB();
+        $queryPrepared = $pdo->prepare("SELECT ID_RECIPE, PICTURE_PATH, TITLE, ID_CREATOR, PSEUDO FROM RECIPES, USER WHERE USER.ID = RECIPES.ID_CREATOR ORDER BY RECIPES.ID_RECIPE DESC;");
+        $queryPrepared->execute();
+        $recipes = $queryPrepared->fetchAll();
+        $pmax = ceil(count($recipes) / 20); //Calcule le nombre de pages totales
+
+
+        if ((($p - 1) * 20 + 19)> count($recipes) -1){
+            $q = count($recipes);
+        }else{
+            $q = ($p - 1) * 20 + 20;
+        }
+
+        for($i = (($p - 1) * 20); $i < $q; $i++){
+            echo '<div class="col-lg-3 col-md-4 col-sm-1 py-3">
+                    <div class="card mb-4 shadow-sm bg-color py-3 px-3 arrondie">
+                        <a href="https://cookit.ovh/recette.php?id='.$recipes[$i]['ID_RECIPE'].'">
+                        <img src="'.$recipes[$i]['PICTURE_PATH'].'" class="card-img-top cardh"> </img>
+                        <div class="card-body text-center arrondie">
+                                    <h4 class="text-white">'.$recipes[$i]['TITLE'].'</h4>
+                                    <a href="https://cookit.ovh/profil.php?id='.$recipes[$i]['ID_CREATOR'].'" class=" btn btn-secondary" style="height : 30px"><p>Créé par '.$recipes[$i]['PSEUDO'].'</p></a>
+                        </div>';
+                        if (isAdmin()){
+                            echo'<div class="text-right">
+                                    <a href="https://cookit.ovh/delRecette.php?id='.$recipes[$i]['ID_RECIPE'].'"><button type="button" class="btn btn-danger px-3"><i class="glyphicon glyphicon-trash" aria-hidden="true"></i></button></a>
+                                </div>';
+                        }
+                        
+                        echo'</a>        
+                    </div>
+                </div>';
+        }
+        ?>
+
+        </div>
+    </div>
+        <!-- PAGINATION --> 
+        <div class="row">
+            <ul class="pagination">
+                <li>
+                <a class="page-link" href="
+                <?php 
+                if ($p == 1){
+                    echo'#';
+                }else{
+                    echo'https://cookit.ovh/index.php?p='.($p -1);
+                }
+                ?>">Previous</a>
+                </li>
+                <li>
+                    <p> Pages : <?=$p .'/'.$pmax ?> </p> 
+                </li>
+                <li class="page-item">
+                <a class="page-link" href="
+                <?php 
+                if ($p == $pmax){
+                    echo'#';
+                }else{
+                    echo'https://cookit.ovh/index.php?p='.($p + 1);
+                }
+                ?>">Next</a>
+                </li>
+            </ul>
+        </div>
+
+    </div>  
+
+
+
+
+
+</div>
+  
         
     </div>
-    
+<?php
+    if(isConnected()){
+    echo '<p id="id-user" hidden="hidden">'.$_SESSION['id'].'</p>';
+    echo '<p id="token-user" hidden="hidden">'.$_SESSION['token'].'</p>';
+}
+
+if(!isset($_GET['p'])){
+    header("Location: index.php?p=1");
+}
+
+$p = $_GET['p'];
+
+?>
+
+
+
+        <!-- <script src="ressources/js/ajax.js"></script> -->
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <?php // include "template/footer.php";?>
+
+
+<!-- PAGINATION --> 
+
