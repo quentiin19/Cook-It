673311a541409235c4b312f7f24b10b0a355d0a5
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-07-01 13:35:55 +0200

    Update profil2.0.php

--- profil2.0.php
+++ profil2.0.php
@@ -5,7 +5,7 @@ include 'template/header.php';
 
 <div class="row">
     <div class=" col-lg-2 col-md-2 col-sm-2 bg-color ">
-        <div class=" h-100 justify-content-center align-items-center">
+        <div class=" h-auto justify-content-center align-items-center">
             <div class="">
                 <a href="#" >Modifier mon profil</a>
             </div>
