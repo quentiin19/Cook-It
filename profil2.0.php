279df3d2e06db6680e2ac19f725d0e43cbab3dd1
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-07-01 16:19:37 +0200

    Update profil2.0.php

--- profil2.0.php
+++ profil2.0.php
@@ -3,10 +3,13 @@
 include 'template/header.php';
 ?>
 
-<div class="row bg-danger " >
-    <div>
+<div class="row " >
+    <div class="col-lg-2 col-md-2 col-sm-2 bg-danger ">
         Blelalleno
     </div>
+    <div class="col-lg-10 col-md-10 col-sm-10 bg-light">
+        bzandknzenf
+    </div>  
 
 
 
