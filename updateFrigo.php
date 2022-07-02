4f41cf044519937e5ff5c328ae0cd5033459ded8
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-07-02 12:34:01 +0200

    Update updateFrigo.php

--- updateFrigo.php
+++ updateFrigo.php
@@ -19,7 +19,9 @@ if (isConnected()){
             $queryPrepared = $pdo->prepare("SELECT COUNT(ID_INGREDIENT) FROM FRIDGE where ID_USER = :id AND ID_INGREDIENT = :ingr;");
             $queryPrepared->execute(['id' => $_SESSION['id'], 'ingr' => $ingredient['ID']]);
             $ingr_fridge = $queryPrepared->fetch();
-            echo $ingr_fridge;
+            echo '<pre>';
+            print_r($ingr_fridge);
+            echo '</pre>';
             if ($ingr_fridge == 0){
                 //Pas dans le frigo
                 echo $ingredient['NAME'].' inséré en bdd';
