41b0bec06d873269966dfec064326336146fed86
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-07-02 10:02:04 +0200

    Update frigo.php

--- frigo.php
+++ frigo.php
@@ -46,45 +46,84 @@ include "template/header.php";
     </div>
 </div>
 
+<?php
+
+if (isConnected() == $_SESSION['id']){
+	$pdo = connectDB();
+	$queryPrepared = $pdo->prepare("SELECT *  FROM RECIPES WHERE ID_RECIPE = :idr;");
+	$queryPrepared->execute(["idr" => $_GET['id']]);
+	$resultR = $queryPrepared->fetch();
+
+	$queryPrepared = $pdo->prepare("SELECT *  FROM NEED WHERE ID_RECIPE = :id;");
+	$queryPrepared->execute(["id" => $_GET['id']]);
+	$resultN = $queryPrepared->fetchAll();
+
+	$queryPrepared = $pdo->prepare("SELECT * FROM INGREDIENTS;");
+	$queryPrepared->execute();
+	$allIngredient = $queryPrepared->fetchAll();
+}
+?>
+
 <div class="col-lg-6 col-md-6 col-sm-6">
     <div class="card bg-color text-white pb-3 my-3" style="border-radius: 1rem;">
-        <h3 class="text-center py-1">Ajouter les ingrédients </h3>
+        <h3 class="text-center py-3">Ingrédients </h3>
 
-        <!--scroll-->
+                <!--clean-->
         <div>
-            <div id="ingredients" class="overflow-auto" style="height : 500px">
+            <div class="overflow-auto " style="height : 300px">
                 <?php
-        $pdo = connectDB();
-
-        $queryPrepared = $pdo->prepare("SELECT * FROM INGREDIENTS;");
-        $queryPrepared->execute();
-        $results = $queryPrepared->fetchAll();
-
-        foreach ($results as $key => $ingredient) { 
-            echo '<div id="'.$ingredient['ID'].'"class="col-lg-12 col-md-12 col-sm-12 background-body arrondie my-2">
-                        <div class="row align-items-center">
-                                <div class="col-lg-1 col-md-1 col-sm-6">
-                                    <input  type="checkbox" name="checkbox'.$ingredient['ID'].'">
-                                </div>
-                                <div class="col-lg-3 col-md-3 col-sm-6">
-                                    <img src="'.$ingredient['PICTURE_PATH'].'" height ="70vh" width="70vw"/>
-                                </div>
-                                <div class="col-lg-3 col-md-3 col-sm-3">
-                                    <p>'.$ingredient['NAME'].'</p>
-                                </div>
-                                <div class="col-lg-3 col-md-2 col-sm-6 ">
-                                    <input class="input-width text-dark" type="text" name="quantity'.$ingredient['ID'].'" placeholder="quantité">
-                                </div>
-                            <div class="col-lg-2 col-md-3 col-sm-3">
-                                    '.$ingredient['UNIT'].'
-                                </div>		
-                        </div>
-                    </div>';
-            
-        }?>
+                $displayed = 0;
+                    foreach($allIngredient as $ingredient){
+                        foreach($resultN as $key => $need){
+                            if($ingredient['ID'] == $need['ID_INGREDIENT']){
+                                $displayed = 1;
+                                echo '<div class="col-lg-12 col-md-12 col-sm-12 background-body arrondie my-2">
+                                        <div class="row align-items-center">
+                                            <div class="col-lg-1 col-md-1 col-sm-6">
+                                                <input checked="checked" type="checkbox" name="checkbox'.$ingredient['ID'].'">
+                                            </div>
+                                            <div class="col-lg-3 col-md-3 col-sm-6">
+                                                <img src="'.$ingredient['PICTURE_PATH'].'" height ="70vh" width="70vw"/>
+                                            </div>
+                                            <div class="col-lg-3 col-md-3 col-sm-3">
+                                                <p>'.$ingredient['NAME'].'</p>
+                                            </div>
+                                            <div class="col-lg-3 col-md-2 col-sm-6 ">
+                                                <input class="input-width text-dark" type="text" name="quantity'.$ingredient['ID'].'" value='.$need['QUANTITY'].' placeholder="quantité">
+                                            </div>
+                                            <div class="col-lg-2 col-md-3 col-sm-3">
+                                                '.$ingredient['UNIT'].'
+                                            </div>		
+                                        </div>
+                                    </div>';
+                            }
+                        }
+                        if($displayed == 0){
+                            echo '<div class="col-lg-12 col-md-12 col-sm-12 background-body arrondie my-2">
+                                    <div class="row align-items-center">
+                                        <div class="col-lg-1 col-md-1 col-sm-6">
+                                            <input type="checkbox" name="checkbox'.$ingredient['ID'].'">
+                                        </div>
+                                        <div class="col-lg-3 col-md-3 col-sm-6">
+                                            <img src="'.$ingredient['PICTURE_PATH'].'" height ="70vh" width="70vw"/>
+                                        </div>
+                                        <div class="col-lg-3 col-md-3 col-sm-3">
+                                            <p>'.$ingredient['NAME'].'</p>
+                                        </div>
+                                        <div class="col-lg-3 col-md-2 col-sm-6 ">
+                                            <input class="input-width" type="text" name="quantity'.$ingredient['ID'].'" placeholder="quantité">
+                                        </div>
+                                        <div class="col-lg-2 col-md-3 col-sm-3">
+                                            '.$ingredient['UNIT'].'
+                                        </div>		
+                                    </div>
+                                </div>';
+                        }
+                        $displayed = 0;
+                    }
+                    ?>
             </div>
         </div>
     </div>
 </div>
-
 <script src='ressources/js/ajax_ingredient.js'></script>
