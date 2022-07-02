e4e39f06d143403a8a6a5635ad3a69fbc5e3546e
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-07-02 12:51:16 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -217,9 +217,7 @@ if (isConnected()){
         
         echo'
         <div class="row">
-            <div class="col-lg-2 col-md-2 col-sm-2"></div>
-            <div class="col-lg-2 col-md-2 col-sm-2"></div>
-            <div class="col-lg-8 col-md-8 col-sm-8">';
+            <div class="col-lg-12 col-md-12 col-sm-12">';
             
         foreach ($results as $result){
             echo '
@@ -236,7 +234,7 @@ if (isConnected()){
         }
         echo '
             </div>
-            <div class="col-lg-2 col-md-2 col-sm-2"></div>
+            
         </div>';
 
 
