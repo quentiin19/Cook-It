84eecc0d1a17c63452d0d0e8b0e290feefc2b129
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-07-02 12:31:27 +0200

    Update updateFrigo.php

--- updateFrigo.php
+++ updateFrigo.php
@@ -4,13 +4,17 @@ require "functions.php";
 $pdo = ConnectDB();
 
 if (isConnected()){
+    echo '<pre>';
+    print_r($_POST);
+    echo '</pre>';
+
 
     $queryPrepared = $pdo->prepare("SELECT * FROM INGREDIENTS;");
     $queryPrepared->execute();
     $ingredients = $queryPrepared->fetchAll();
 
     foreach($ingredients as $ingredient){
-        if (isset($_POST['nfcheckbox'.$ingredient['ID']])){
+        if(isset($_POST['nfcheckbox'.$ingredient['ID']])){
             // il a coché la check
             $queryPrepared = $pdo->prepare("SELECT COUNT(ID_INGREDIENT) FROM FRIDGE where ID_USER = :id AND ID_INGREDIENT = :ingr;");
             $queryPrepared->execute(['id' => $_SESSION['id'], 'ingr' => $ingredient['ID']]);
@@ -20,9 +24,9 @@ if (isConnected()){
                 $queryPrepared = $pdo->prepare("INSERT INTO FRIDGE VALUES (:id, :ingr, :quant);");
                 $queryPrepared->execute(['id' => $_SESSION['id'], 'ingr' => $ingredient['ID'], 'quant' => $_POST['nfquantity'.$ingredient['ID']] ]);
             }
-        }elseif (isset($_POST['fcheckbox'.$ingredient['ID']])){
+        }elseif(isset($_POST['fcheckbox'.$ingredient['ID']])){
 
-            if (($_POST['fquantity'.$ingredient['ID']]) > 0) {
+            if(($_POST['fquantity'.$ingredient['ID']]) > 0) {
                     $queryPrepared = $pdo -> prepare("UPDATE FRIDGE SET QUANTITY = :quantity WHERE ID_INGREDIENT = :ingr AND ID_USER = :id");
                     $queryPrepared =execute(['quantity' => $_POST['fquantity'.$ingredient['ID']], 'ingr' =>  $ingredient['ID'], 'id' => $_SESSION[['id']]]);
                 }
@@ -40,7 +44,7 @@ if (isConnected()){
     }
 }
 
-header("Location: frigo.php?id=".$_SESSION['id']);
+//header("Location: frigo.php?id=".$_SESSION['id']);
 // si nf et dans dans le frigo = erreuf
 // si nf pas dans le frigo et checkbox = go dans le frigo
 // si f et que <> checkbox = delete from ffridge
