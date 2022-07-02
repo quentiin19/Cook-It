e66b7d0bf99a3d7b37660e75eb8f16253a58f7bf
OussBth <103314967+OussBth@users.noreply.github.com>
2022-07-02 10:44:21 +0200

    changemail

--- modif_email.php
+++ modif_email.php
@@ -8,7 +8,7 @@ include "template/header.php";
     <div class="col-lg-2 col-md-2 col-sm-2 bg-color my-3 ml-5 arrondie ">
     <div class="row">
                 <div class="col-lg-12 my-5 py-2 pl-2">
-                    <a class="text-white" href="#" >Modifier mon profil</a>
+                    <a class="text-white" href="https://cookit.ovh/modif_profil.php?id=<?= $_SESSION['id']?>" >Modifier mon profil</a>
                 </div>
             </div>
             <div class="row">
@@ -23,7 +23,7 @@ include "template/header.php";
             </div>
             <div class="row">
                 <div class="col-lg-12 my-4 py-2 pl-2">
-                    <a class="text-white" href="#" >Modifier mon mot de passe</a>
+                    <a class="text-white" href="https://cookit.ovh/pwdmodif.php?id=<?= $_SESSION['id']?>" >Modifier mon mot de passe</a>
                 </div>
             </div>
             <div class="row">
