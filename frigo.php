23c4084b6005ecf6830bf4975cbf544dd8d312da
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-07-02 13:16:39 +0200

    Update frigo.php

--- frigo.php
+++ frigo.php
@@ -72,6 +72,7 @@ if (isConnected() == $_SESSION['id']){
                 echo 'eee<pre>';
                 print_r($ingredient);
                 echo '</pre>';
+                echo $ingredient['QUANTITY'];
             echo '<div id="'.$ingredient['ID'].'"class="col-lg-12 col-md-12 col-sm-12 background-body arrondie my-2">
                         <div class="row align-items-center">
                                 <div class="col-lg-1 col-md-1 col-sm-6">
