0fe257615a7f81ac8c7c19cdc623734e9b292936
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-07-02 09:48:49 +0200

    Update AddRecette.php

--- AddRecette.php
+++ AddRecette.php
@@ -41,7 +41,7 @@
                                                 <!--scroll-->
                                                 <div>
 													<input type="text" id="search-bar-ingredient" class="text-dark" placeholder="rechercher un ingredient">
-                                                    <div id="ingredients" class="overflow-auto" style="height : 300px">
+                                                    <div id="ingredients" class="overflow-auto" style="height : 400px">
                                                         <?php
 												$pdo = connectDB();
 
