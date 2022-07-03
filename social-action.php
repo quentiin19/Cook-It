3dccc1c54a5849c5e5eb5c4e6b372526df73d09d
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-07-03 11:18:31 +0200

    e

--- social-action.php
+++ social-action.php
@@ -103,15 +103,18 @@ if (isConnected() == $_SESSION['id']) {
                 break;
         
             case 'sub':
+                echo 'sub';
                 if (isset($statesub1[0])){
                     if ($statesub1[0] == 1) {
                         break;
                     }else{
+                        echo'update';
                         //mettre 1 en bdd
                         $queryPrepared = $pdo->prepare("UPDATE SUBSCRIPTION SET STATUS = 1 WHERE ID_SUBSCRIBER = :id_sender; AND ID_SUBSCRIPTION = :id_receveur;");
                         $queryPrepared->execute(['id_sender'=>$_SESSION['id'], 'id_receveur'=>$_GET['id']]);
                     }
                 }else{
+                    echo 'insert';
                     //mettre 1 en bdd
                     $queryPrepared = $pdo->prepare("INSERT INTO SUBSCRIPTION VALUES (:id_sender, :id_receveur, 1, NOW());");
                     $queryPrepared->execute(['id_sender'=>$_SESSION['id'], 'id_receveur'=>$_GET['id']]);
@@ -140,9 +143,9 @@ if (isConnected() == $_SESSION['id']) {
                 break;
         }
     }else{
-        header("Location: login.php");
+        //header("Location: login.php");
     }
 }else{
-    header("Location: login.php");
+    //header("Location: login.php");
 }
-header("Location: profil.php?id=".$id);
+//header("Location: profil.php?id=".$id);
