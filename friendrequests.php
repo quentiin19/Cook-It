d594cc7aace0a9d2c4ae3bab38950de848c7088d
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-07-03 21:26:41 +0200

    Update friendrequests.php

--- friendrequests.php
+++ friendrequests.php
@@ -20,7 +20,7 @@ if (isConnected() != $_SESSION['id']) {
         <?php
         foreach ($matchs as $match) {
             $queryPrepared = $pdo->prepare("SELECT STATUS FROM MATCHS WHERE ID_MATCHER = :id AND ID_MATCH = :id_sender;");
-            $queryPrepared->execute(['id' => $_SESSION['id'], 'id_sender' => $matchs['ID']]);
+            $queryPrepared->execute(['id' => $_SESSION['id'], 'id_sender' => $match['ID']]);
             $result = $queryPrepared->fetch();
 
             //si nous n'avons pas encore matché avec cette personne
