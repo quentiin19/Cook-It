991c072b7cd6a125cc76edc8952b282f73da505f
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-07-03 12:23:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,47 @@ if(!isset($_GET['p'])){
 $p = $_GET['p'];
 ?>
 
+<div class="d-flex justify-content-start my-3"> 
+  <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#myModal">
+    Comment ça marche ?
+  </button>
+</div>
+
+<!-- The Modal -->
+<div class="modal" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Comment ça marche ?</h4>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body">
+        Bienvenue sur CookIt, le site de cuisine pas comme les autres, </br>
+        Sur cet incroyable site, vous pourrez créer vos recettes favorites et les partagées au monde entier.</br>
+        Votre Frigo est presque vide, vous ne savez pas quoi cuisiner ? </br>
+        Pas de panique , sélectionnez les ingrédients et la quantité qu'il vous reste dans la section "frigo",
+        et nous vous proposerons des recettes.</br>
+        Si un utilisateur publie du contenu qui vous plait vous pouvez le suivre grâce au bouton "S'abonner".</br>
+        N'oubliez pas d'ajouter une description qui vous correspond à votre profil.</br>
+        La particularité de Cook'it est le systeme de "match" ! </br>
+        Lorsque qu'une personne vous interesse ( vous avez les memes gouts cullinaires, et que sa description vous plait ),
+        Il suffit de cliquer sur "match", sur son profil. </br>
+        Si les 2 personnes match alors une messagerie 
+        est débloquer pour faire plus ample connaissance et pourquoi trouver votre ame-soeur.  
+      </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Fermer</button>
+      </div>
+
+    </div>
+  </div>
+</div>
 
 
 
@@ -109,47 +150,7 @@ $p = $_GET['p'];
                 
             </div>
         </div>
-<div class="d-flex justify-content-center my-3"> 
-  <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#myModal">
-    Comment ça marche ?
-  </button>
-</div>
 
-<!-- The Modal -->
-<div class="modal" id="myModal">
-  <div class="modal-dialog">
-    <div class="modal-content">
-
-      <!-- Modal Header -->
-      <div class="modal-header">
-        <h4 class="modal-title">Comment ça marche ?</h4>
-        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-      </div>
-
-      <!-- Modal body -->
-      <div class="modal-body">
-        Bienvenue sur CookIt, le site de cuisine pas comme les autres, </br>
-        Sur cet incroyable site, vous pourrez créer vos recettes favorites et les partagées au monde entier.</br>
-        Votre Frigo est presque vide, vous ne savez pas quoi cuisiner ? </br>
-        Pas de panique , sélectionnez les ingrédients et la quantité qu'il vous reste dans la section "frigo",
-        et nous vous proposerons des recettes.</br>
-        Si un utilisateur publie du contenu qui vous plait vous pouvez le suivre grâce au bouton "S'abonner".</br>
-        N'oubliez pas d'ajouter une description qui vous correspond à votre profil.</br>
-        La particularité de Cook'it est le systeme de "match" ! </br>
-        Lorsque qu'une personne vous interesse ( vous avez les memes gouts cullinaires, et que sa description vous plait ),
-        Il suffit de cliquer sur "match", sur son profil. </br>
-        Si les 2 personnes match alors une messagerie 
-        est débloquer pour faire plus ample connaissance et pourquoi trouver votre ame-soeur.  
-      </div>
-
-      <!-- Modal footer -->
-      <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Fermer</button>
-      </div>
-
-    </div>
-  </div>
-</div>
 
 
 
