8f5bb977c55582f4dd6f3f2838946b5a77dd3d9b
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-07-03 13:15:30 +0200

    Update api_fridge_recipe.php

--- ressources/api/api_fridge_recipe.php
+++ ressources/api/api_fridge_recipe.php
@@ -8,12 +8,17 @@ include '../../functions.php';
 
 $id = $_GET['id'];
 $dif = $_GET['dif'];
+
 //$token = $_GET['token'];
 
 
 function returnRecipes($difficulty, $id){
+    $missingIngr = 0;
+    $found = 0;
+    $recipes_found = array();
+
     $pdo = connectDB();
-    $query = $pdo->prepare("SELECT ID_INGREDIENT FROM FRIDGE WHERE ID_USER = :id;");
+    $query = $pdo->prepare("SELECT * FROM FRIDGE WHERE ID_USER = :id;");
     $query->execute(['id'=>$id]);
     $ingredients = $query->fetchAll();
 
@@ -33,8 +38,21 @@ function returnRecipes($difficulty, $id){
         $query->execute(['id'=>$recipe['ID_RECIPE']]);
         $needs = $query->fetchAll();
 
+        foreach ($needs as $key => $need) {
+            foreach ($ingredients as $key => $ingredient) {
+                if ($ingredient['ID'] == $need['ID_INGREDIENT']) {
+                    if ($ingredient['QUANTITY'] >= $need['QUANTITY']) {
+                        $found += 1;
+                    }
+                }
+            }
+        }
 
+        if ($found >= (count($needs) - $dif)) {
+            array_push($recipes_found, $recipe);
+        }
     }
+    echo json_encode($recipes_found);
 }
 
 
