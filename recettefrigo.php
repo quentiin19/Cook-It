c2a3b5ad86cd1f4adab8ae07d0cb98ebd2a9060f
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-07-03 12:34:14 +0200

    api

---
+++ recettefrigo.php
@@ -0,0 +1,14 @@
+<?php
+include 'template/header.php';
+
+
+if(isConnected() == $_GET['id']){
+    echo '<p hidden="hidden" id="id-user">'.$_GET['id'].'</p>';
+}
+
+?>
+
+<div id='recettes'></div>
+
+
+<script src="ressources/js/fridgerecipe.js"></script>
---
+++ ressources/api/api_fridge_recipe.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+include '../../functions.php';
+
+header("Content-type: application/json");
+
+
+$pdo = connectDB();
+$id = $_GET['id'];
+$dif = $_GET['dif'];
+$token = $_GET['token'];
+
+
+function returnRecipes($difficulty, $id){
+    $query = $pdo->prepare("SELECT ID_INGREDIENT FROM FRIDGE WHERE ID_USER = :id;");
+    $query->execute(['id'=>$id]);
+    $ingredients = $query->fetchAll();
+
+
+    $query = $pdo->prepare("SELECT ID_INGREDIENT FROM NEED WHERE ID_RECIPE IN (SELECT ID_RECIPE FROM RECIPES);");
+    $query->execute(['id'=>$id]);
+    $ingredients = $query->fetchAll();
+
+    echo '<pre>';
+    print_r($ingredients);
+    echo '</pre>';
+
+}
+
+
+$query = $pdo->prepare("SELECT TOKEN FROM USER WHERE ID = :id;");
+$query->execute(['id'=> $id]);
+$tokenbdd = $query->fetch();
+
+
+
+if ($tokenbdd[0] == $token) {
+    echo returnRecipes($dif, $id)
+}
+echo returnRecipes($dif, $id)
