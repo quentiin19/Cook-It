4d03379485e8ca18acccab3fe2c340be4f80fa7b
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-07-03 17:02:01 +0200

    Update lightmode.js

--- lightmode.js
+++ lightmode.js
@@ -13,40 +13,4 @@ const btn = document.getElementById('switch-mode');
 
 const div = document.querySelectorAll("bg-color");
 
-<<<<<<< HEAD
 console.log(div);
-=======
-btn.addEventListener('click', function () {
-    document.body.classList.toggle('bg-color-light');
-    let theme = "dark";
-
-    if (document.body.classList.contains('bg-color')) {
-        theme = 'dark';
-        switchTheme('bg-color-light', 'bg-color');
-        
-    } else {
-        theme = "light";
-        switchTheme('bg-color', 'bg-color-light');
-    }
-    localStorage.setItem('theme', theme);
-})
-
-
-const localTheme = localStorage.getItem('theme');
-
-if (localTheme == 'light') {
-    document.body.classList.add('light-theme');
-    btn.setAttribute('checked', 'true');
-    switchTheme('text-dark', 'text-light');
-
-}
-if (localTheme == 'dark') {
-    document.body.classList.remove('light-theme');
-    btn.removeAttribute('bg-color');
-    switchTheme('text-light', 'text-dark');
-
-}
->>>>>>> 15d0e9826cb3c8d3484ea396bd00a182ad0526a0
-
-
-
