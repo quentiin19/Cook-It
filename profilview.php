16a1866facca96d45b1c9d9bd11bd045ff8ded91
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-07-03 17:02:06 +0200

    Update profilview.php

--- profilview.php
+++ profilview.php
@@ -206,6 +206,9 @@ if (isConnected()){
                         <div class="col-lg-4">
                             <a class="text-white" href="<?= 'https://cookit.ovh/viewsub.php?id='.$_GET['id'].'&display=1'?>"><h4>Abonnement : <?= $abonnement[0]?></h4><a> 
                         </div>
+                        <div class="col-lg-4 ">
+                            <h4>Recettes : <?= $nbrecipe[0]?></h4>
+                        </div>
                         <div class="col-lg-4">
                             <a class="text-white" href="<?= 'https://cookit.ovh/viewsub.php?id='.$_GET['id'].'&display=2'?>"><h4>Abonnés : <?= $abonnes[0]?></h4><a>
                         </div>
