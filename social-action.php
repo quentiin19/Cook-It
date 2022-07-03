56c51a754c3631aa4ca1d27323acb3154de718c0
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-07-03 13:43:06 +0200

    Update social-action.php

--- social-action.php
+++ social-action.php
@@ -40,9 +40,9 @@ if (isConnected() == $_SESSION['id']) {
         switch ($action) {
             case 'match':
                 if (isset($statematch1[0])){
-                    if ($statematch2[0] == -1 || ($statematch1[0] == 2) || ($statematch2[0] == 2) || ($statematch1[0] == 1 && $statematch1[0] == 1)) {
+                    if ($statematch2[0] == -1 || ($statematch1[0] == 2) || ($statematch2[0] == 2)) {
                         break;
-                    }elseif($statematch1[0] == 1 || $statematch2[0] == 1){
+                    }elseif($statematch1[0] == 1 && $statematch2[0] == 1){
                         //mettre 2 en bdd
                         $queryPrepared = $pdo->prepare("UPDATE MATCHS SET STATUS = 2 WHERE ID_MATCHER = :id_sender; AND ID_MATCH = :id_receveur;");
                         $queryPrepared->execute(['id_sender'=>$_SESSION['id'], 'id_receveur'=>$_GET['id']]);
@@ -109,6 +109,7 @@ if (isConnected() == $_SESSION['id']) {
                     }else{
                         //mettre 1 en bdd
                         $queryPrepared = $pdo->prepare("UPDATE SUBSCRIPTION SET STATUS = 1 WHERE ID_SUBSCRIBER = :id_sender AND ID_SUBSCRIPTION = :id_receveur;");
+                        $queryPrepared->execute(['id_sender'=>$_SESSION['id'], 'id_receveur'=>$_GET['id']]);
                     }
                 }else{
                     //mettre 1 en bdd
