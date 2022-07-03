bf1ee409f82dbc039825be1dccd62cd623bc6fc7
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-07-03 13:30:11 +0200

    Update lightmode.js

--- lightmode.js
+++ lightmode.js
@@ -1,55 +1,46 @@
-// function switchTheme(theme_add, theme_rm) {
-//     const lightText = document.body.querySelectorAll(.${theme_rm});
 
-//     for (let index = 0; index < lightText.length; index++) {
-//         const element = lightText[index];
-//         element.classList.remove(background-body);
-//         element.classList.add(bg-light);
-//     }
-// }
+function switchTheme(theme_add, theme_rm) {
+    const lightText = document.body.querySelectorAll(theme_rm);
 
-function switchCards(theme) {
-    const cards = document.querySelectorAll('.film');
-    for (let index = 0; index < cards.length; index++) {
-        const element = cards[index];
-        if (theme == 'light') {
-            element.classList.remove('custom-cards');
-        }
-        if (theme == 'dark') {
-            element.classList.add('custom-cards');
-        }
+    for (let index = 0; index < lightText.length; index++) {
+        const element = lightText[index];
+        element.classList.remove(theme_rm);
+        element.classList.add(theme_add);
     }
 }
 
-const btn = document.querySelector('#darkSwitch');
+
+const btn = document.querySelector('#light-mode');
+
+
+btn.addEventListener('click', function () {
+    document.body.classList.toggle('bg-color');
+    let theme = "dark";
+
+    if (document.body.classList.contains('bg-color')) {
+        theme = 'dark';
+        switchTheme('bg-color-light', 'bg-color');
+        
+    } else {
+        theme = "light";
+        switchTheme('bg-color', 'bg-color-light');
+    }
+    localStorage.setItem('theme', theme);
+})
+
+
 const localTheme = localStorage.getItem('theme');
 
 if (localTheme == 'light') {
     document.body.classList.add('light-theme');
     btn.setAttribute('checked', 'true');
     switchTheme('text-dark', 'text-light');
-    switchCards('light');
+
 }
 if (localTheme == 'dark') {
     document.body.classList.remove('light-theme');
-    btn.removeAttribute('checked');
+    btn.removeAttribute('bg-color');
     switchTheme('text-light', 'text-dark');
-    switchCards('dark');
+
 }
 
-
-btn.addEventListener('click', function () {
-    document.body.classList.toggle('light-theme');
-    let theme = "dark";
-
-    if (document.body.classList.contains('light-theme')) {
-        theme = "light";
-        switchTheme('text-dark', 'text-light');
-        switchCards('light');
-    } else {
-        theme = 'dark';
-        switchTheme('text-light', 'text-dark');
-        switchCards('dark');
-    }
-    localStorage.setItem('theme', theme);
-})
