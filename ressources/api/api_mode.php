ae8dde0ecee1dc68d49640b407ee5123981c85aa
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-07-03 17:52:09 +0200

    Update api_mode.php

--- ressources/api/api_mode.php
+++ ressources/api/api_mode.php
@@ -2,8 +2,13 @@
 require '../../functions.php';
 header("Content-type: application/json");
 
+$pdo = connectDB();
+
+$action = $_GET['action'];
+$id = $_GET['id'];
+
+
 function returnMode($id){
-    $pdo = connectDB();
     $query = $pdo->prepare("SELECT MODE FROM USER WHERE ID = :id;");
     $query->execute(['id'=>$id]);
     $result = $query->fetch();
@@ -11,4 +16,23 @@ function returnMode($id){
     return json_encode($result);
 }
 
-echo returnMode($_GET['id']);
+function changeMode($id){
+    $mode = returnMode($id);
+
+    if($mode == 1){
+        $query = $pdo->prepare("UPDATE USER SET MODE = 0 WHERE ID = :id;");
+        $query->execute(['id'=>$id]);
+        return json_encode(0);
+    }else{
+        $query = $pdo->prepare("UPDATE USER SET MODE = 1 WHERE ID = :id;");
+        $query->execute(['id'=>$id]);
+        return json_encode(1);
+    }
+}
+
+
+if($action == 'change'){
+    echo changeMode($id)
+}elseif ($action == 'get') {
+    echo returnMode($id);
+}
