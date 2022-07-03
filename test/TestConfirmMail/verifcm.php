e64b3aeed58159772f069ef8e3e75e36179a06e8
OussBth <103314967+OussBth@users.noreply.github.com>
2022-07-03 11:29:03 +0200

    Update verifcm.php

--- test/TestConfirmMail/verifcm.php
+++ test/TestConfirmMail/verifcm.php
@@ -4,13 +4,14 @@ require "../../functions.php";
 
 print_r($_SESSION);
 print_r($_GET);
+print_r($_POST);
 
 if($_GET['id'] == $_SESSION['id']){
     if($_GET['cle'] == $_SESSION['cle']){
         $pdo = connectDB();
         
         $queryPrepared = $pdo->prepare("UPDATE USER SET MAIL = :mail where USER.ID = :id");
-	    $queryPrepared->execute(["id"=>$_SESSION['id'], "mail" => $_POST['emailnew']]);
+	    $queryPrepared->execute(["mail" => $_POST['emailnew'], "id"=>$_SESSION['id']]);
         echo "Vous avez bien validé votre mail, cliquez sur le lien ci dessous pour vous connecter";
         echo "<brs><a href=http://51.255.172.36/login.php>Se Connecter</a>";
     }else{
