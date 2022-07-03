6b90488db002cdf5ca69ca97958819b20ee33bc8
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-07-03 11:41:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,36 +13,7 @@ if(!isset($_GET['p'])){
 $p = $_GET['p'];
 ?>
 
-<div class="container mt-3"> 
-  <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#myModal">
-    Comment ça marche ?
-  </button>
-</div>
 
-<!-- The Modal -->
-<div class="modal" id="myModal">
-  <div class="modal-dialog">
-    <div class="modal-content">
-
-      <!-- Modal Header -->
-      <div class="modal-header">
-        <h4 class="modal-title">Modal Heading</h4>
-        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-      </div>
-
-      <!-- Modal body -->
-      <div class="modal-body">
-        Modal body..
-      </div>
-
-      <!-- Modal footer -->
-      <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-      </div>
-
-    </div>
-  </div>
-</div>
 
 
 
@@ -138,6 +109,36 @@ $p = $_GET['p'];
                 
             </div>
         </div>
+        <div class="container mt-3"> 
+  <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#myModal">
+    Comment ça marche ?
+  </button>
+</div>
+
+<!-- The Modal -->
+<div class="modal" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Modal Heading</h4>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body">
+        Modal body..
+      </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+      </div>
+
+    </div>
+  </div>
+</div>
 
 
 
