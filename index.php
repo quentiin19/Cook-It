8d25730ba220d4184e7ca7193e027306f5dc33af
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-07-03 12:20:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -131,13 +131,14 @@ $p = $_GET['p'];
         Bienvenue sur CookIt, le site de cuisine pas comme les autres, </br>
         Sur cet incroyable site, vous pourrez créer vos recettes favorites et les partagées au monde entier.</br>
         Votre Frigo est presque vide, vous ne savez pas quoi cuisiner ? </br>
-        Pas de panique , sélectionnez les ingrédients et la quantité qu'il vous reste dans la section "frigo",</br>
+        Pas de panique , sélectionnez les ingrédients et la quantité qu'il vous reste dans la section "frigo",
         et nous vous proposerons des recettes.</br>
         Si un utilisateur publie du contenu qui vous plait vous pouvez le suivre grâce au bouton "S'abonner".</br>
         N'oubliez pas d'ajouter une description qui vous correspond à votre profil.</br>
         La particularité de Cook'it est le systeme de "match" ! </br>
-        Lorsque qu'une personne vous interesse ( vous avez les memes gouts cullinaires, et que sa description vous plait ),</br>
-        Sur son profil, il suffit de cliquer sur "match". Si les 2 personnes match alors une messagerie 
+        Lorsque qu'une personne vous interesse ( vous avez les memes gouts cullinaires, et que sa description vous plait ),
+        Il suffit de cliquer sur "match", sur son profil. </br>
+        Si les 2 personnes match alors une messagerie 
         est débloquer pour faire plus ample connaissance et pourquoi trouver votre ame-soeur.  
       </div>
 
