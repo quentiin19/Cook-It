9f2f786b08340dc46cbe6497cebae4cd0d8aeea5
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-07-03 11:01:06 +0200

    Update friendrequests.php

--- friendrequests.php
+++ friendrequests.php
@@ -19,6 +19,7 @@ if (isConnected() != $_SESSION['id']){
             foreach ($friends as $friend){
                 echo '<div class="col-lg-3 col-md-4 col-sm-6"> 
                         <div class=" card bg-color text-center shadow p-3 mb-5 rounded">
+                            <img src="'.$friend['PATH_AVATAR'].'">
                             <div>'.$friend['PSEUDO'].'</div>
                             <button type="button" class="btn btn"><a href="https://cookit.ovh/friendRaccepted.php?id='.$friend['ID'].'" class ="bg-light rounded my-3"> Accepter</a></button>
                             <button type="button" class="btn btn"><a href="https://cookit.ovh/friendRrefused.php?id='.$friend['ID'].'" class ="bg-light rounded my-3"> Refuser</a></button>
