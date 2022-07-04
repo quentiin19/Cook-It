c20bfad1241c75f9113306cd0378f3a20b98d595
quentiin19 <81647755+quentiin19@users.noreply.github.com>
2022-07-04 11:32:45 +0200

    Update mdpforget.php

--- mdpforget.php
+++ mdpforget.php
@@ -1,19 +1,35 @@
 <?php
 require "./template/header.php";
 ?>
-<div class="col-lg-6 col-md-12 col-sm-12">
+<div class="col-lg-12 col-md-12 col-sm-12 d-flex justify-content-center h-auto arrondie  ">
+    <div class="container py-2  h-auto  ">
+        <div class="row d-flex justify-content-center align-items-center h-100">
 
-    <form method="POST" action="">
-        <input type="password" class="form-control" name="password" placeholder="Votre Mot de passe" required="required"><br>
-        <input type="password" class="form-control" name="passwordconfirm" placeholder="Confirmez votre Mot de passe" required="required"><br>
-        <div class="row">
-            <div class="col-lg-4 col-md-1 col-sm-0"></div>
-            <div class="col-lg-4 col-md-12 col-sm-12">
-                <input type="submit" class="btn btn-outline-light btn-lg py-2 " value="Envoyer">
+            <div class="card bg-color text-white" style="border-radius: 1rem;">
+                <div class="card-body  text-center">
+                    <div class="mb-md-5 mt-md-4 pb-5">
+
+                        <h2 class="fw-bold mb-2 text-uppercase">Problèmes de connexion ?</h2>
+                        <p class="text-white-50 mb-5"> Entrez votre adresse mail et nous vous enverrons un lien pour récupérer votre compte.</p>
+                            <div class="col-lg-6 col-md-12 col-sm-12">
+
+                                <form method="POST" action="">
+                                    <input type="password" class="form-control" name="password" placeholder="Votre Mot de passe" required="required"><br>
+                                    <input type="password" class="form-control" name="passwordconfirm" placeholder="Confirmez votre Mot de passe" required="required"><br>
+                                    <div class="row">
+                                        <div class="col-lg-4 col-md-1 col-sm-0"></div>
+                                        <div class="col-lg-4 col-md-12 col-sm-12">
+                                            <input type="submit" class="btn btn-outline-light btn-lg py-2 " value="Envoyer">
+                                        </div>
+                                        <div class="col-lg-4 col-md-1 col-sm-0"></div>
+                                    </div>
+                                </form>
+                            </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-4 col-md-1 col-sm-0"></div>
         </div>
-    </form>
+    </div>
 </div>
 
 <?php include "template/footer.php"; ?>
