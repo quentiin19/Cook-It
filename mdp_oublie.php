cb55e41b91dbb8c3f32b188803af45d48244eba9
OussBth <103314967+OussBth@users.noreply.github.com>
2022-07-04 11:52:06 +0200

    Update mdp_oublie.php

--- mdp_oublie.php
+++ mdp_oublie.php
@@ -90,7 +90,7 @@ if(isset($_POST['email'])){
 			$subj = 'Mot de passe oublié';
 			$msg = '<a href="http://51.255.172.36/mdpforget.php?id='.$result['ID'].'&cle='.$cle.'">Confirmer</a><h1>Confirmez le mail en cliquant sur le lien ci dessus</h1>';
 			smtpmailer($email, $from, $name, $subj, $msg);
-			//header("Location:login.php");
+			header("Location:login.php");
 			print_r($_SESSION);
 		}
 		else {
