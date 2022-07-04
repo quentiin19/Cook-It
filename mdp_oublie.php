9415e7a372c29630f8f6d7e6ffeade12cc7cf161
BonziSki <103522094+BonziSki@users.noreply.github.com>
2022-07-04 11:07:16 +0200

    Update mdp_oublie.php

--- mdp_oublie.php
+++ mdp_oublie.php
@@ -84,7 +84,7 @@ if(isset($_POST['email'])){
 		$queryPrepared->execute(["email" => $email]);
 		$result = $queryPrepared->fetch();
 		echo("récup du mail en bdd");
-		if ($result['ID'] == $email) {
+		if (isset($result['ID'])) {
 			$_SESSION['cle'] = $cle;
 			echo("envois mail");
 			//envois du mail
